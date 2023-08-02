9bc1a0c888b492173381fe5fbcf7d2fca307e0e7
Michael Edgar <medgar@redhat.com>
2023-08-03 05:05:15 +0800

    Add dependabot for actions, remove unused test classes (#2)
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- src/test/java/com/github/eyefloaters/console/kafka/systemtest/TestTag.java
+++
@@ -1,6 +0,0 @@
-package com.github.eyefloaters.console.kafka.systemtest;
-
-public class TestTag {
-    public static final String OAUTH = "oauth";
-    public static final String PLAIN = "plain";
-}
