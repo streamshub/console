dcf3bcde38a01bfaa91e8fa139639b61a04b03cb
Riccardo Forina <rforina@redhat.com>
2023-09-21 15:32:39 +0200

    Use the formatting from the convert library instead of the browser intl

--- ui/utils/format.ts
+++ ui/utils/format.ts
@@ -4,20 +4,9 @@ import { useFormatter } from "next-intl";
 export function useFormatBytes() {
   const format = useFormatter();
   return function formatBytes(bytes: number) {
-    const res = convert(bytes, "bytes").to("best", "metric");
-    const unit: Record<(typeof res)["unit"], string> = {
-      bits: "bit",
-      GB: "gigabyte",
-      TB: "terabyte",
-      PB: "petabyte",
-      MB: "megabyte",
-      KB: "kilobyte",
-      B: "byte",
-    };
-    return format.number(res.quantity, {
-      style: "unit",
-      unit: unit[res.unit],
-      unitDisplay: "narrow",
-    });
+    const res = convert(bytes, "bytes").to("best", "imperial");
+    return `${format.number(res.quantity, {
+      style: "decimal",
+    })} ${res.unit}`;
   };
 }
