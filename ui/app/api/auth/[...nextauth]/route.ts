51424045acf13324f239ea18044a3d57c2ff9624
Riccardo Forina <rforina@redhat.com>
2023-10-04 17:11:04 +0200

    revisited refresh logic

--- ui/app/api/auth/[...nextauth]/route.ts
+++ ui/app/api/auth/[...nextauth]/route.ts
@@ -27,11 +27,11 @@ export const authOptions: AuthOptions = {
       let tokenExpiration = new Date(
         (typeof token?.expires_at === "number" ? token.expires_at : 0) * 1000,
       );
-      log.debug("Token expiration, expires_at:", tokenExpiration);
+      log.trace({ tokenExpiration }, "Token expiration");
 
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
-        log.debug("account present, saving new token");
+        log.trace("account present, saving new token");
         // Save the access token and refresh token in the JWT on the initial login
         return {
           access_token: account.access_token,
@@ -41,50 +41,62 @@ export const authOptions: AuthOptions = {
       }
 
       if (Date.now() < tokenExpiration.getTime()) {
-        log.debug("Token not yet expired");
+        log.trace("Token not yet expired");
         // If the access token has not expired yet, return it
-        return token;
-      } else {
-        log.debug("Token has expired");
         let refresh_token =
           typeof token.refresh_token === "string" ? token.refresh_token : "";
 
-        const params = new URLSearchParams({
+        const params = {
           client_id: keycloak.options?.clientId ?? "",
           client_secret: keycloak.options?.clientSecret ?? "",
           grant_type: "refresh_token",
           refresh_token: refresh_token,
-        });
+        };
+
+        log.trace(
+          {
+            url: tokenEndpoint.value,
+            body: params,
+          },
+          "Refreshing token",
+        );
 
         try {
           const response = await fetch(tokenEndpoint.value, {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: params,
+            body: new URLSearchParams(params),
             method: "POST",
           });
 
-          const tokens: TokenSet = await response.json();
-
+          const refreshToken: TokenSet = await response.json();
           if (!response.ok) {
-            throw tokens;
+            throw refreshToken;
           }
+          log.trace(refreshToken, "Got refresh token");
 
           let expires_in =
-            typeof tokens.expires_in === "number" ? tokens.expires_in : -1;
+            typeof refreshToken.expires_in === "number"
+              ? refreshToken.expires_in
+              : -1;
 
-          return {
+          const newToken = {
             ...token, // Keep the previous token properties
-            access_token: tokens.access_token,
+            access_token: refreshToken.access_token,
             expires_at: Math.floor(Date.now() / 1000 + expires_in),
             // Fall back to old refresh token, but note that
             // many providers may only allow using a refresh token once.
-            refresh_token: tokens.refresh_token ?? token.refresh_token,
+            refresh_token: refreshToken.refresh_token ?? token.refresh_token,
           };
+          log.trace(newToken, "New token");
+          return newToken;
         } catch (error) {
-          log.error("Error refreshing access token", error);
+          log.error(error, "Error refreshing access token");
           // The error property will be used client-side to handle the refresh token error
           return { ...token, error: "RefreshAccessTokenError" as const };
         }
+      } else {
+        log.trace("Token has expired");
+        return null;
       }
     },
     async session({ session, token }) {
