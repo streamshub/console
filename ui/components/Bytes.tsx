8301c960d765de229fdc3d3403d75a11f4980cdc
Michael Edgar <medgar@redhat.com>
2023-10-10 11:49:56 -0400

    Display any defined number value
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/components/Bytes.tsx
+++ ui/components/Bytes.tsx
@@ -2,5 +2,5 @@ import { useFormatBytes } from "@/utils/format";
 
 export function Bytes({ value }: { value: number | undefined }) {
   const formatter = useFormatBytes();
-  return value ? formatter(value) : "-";
+  return (typeof value !== 'undefined') ? formatter(value) : "-";
 }
