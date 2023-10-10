91e0db958d651d97c009557fb134d67f28bf2e09
Michael Edgar <michael@xlate.io>
2023-10-10 13:57:13 -0400

    Compare directly with `undefined`
    Co-authored-by: Riccardo Forina <rforina@redhat.com>

--- ui/components/Bytes.tsx
+++ ui/components/Bytes.tsx
@@ -2,5 +2,5 @@ import { useFormatBytes } from "@/utils/format";
 
 export function Bytes({ value }: { value: number | undefined }) {
   const formatter = useFormatBytes();
-  return (typeof value !== 'undefined') ? formatter(value) : "-";
+  return (value !== undefined) ? formatter(value) : "-";
 }
