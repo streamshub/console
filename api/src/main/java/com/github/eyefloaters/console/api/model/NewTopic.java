3e9c5852b7e04f6e1f87131d038e4ee44e1be3b4
Michael Edgar <medgar@redhat.com>
2023-11-22 16:47:41 -0500

    fix: validate new topic name is not blank (#210)
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- api/src/main/java/com/github/eyefloaters/console/api/model/NewTopic.java
+++ api/src/main/java/com/github/eyefloaters/console/api/model/NewTopic.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -43,7 +44,7 @@ import static com.github.eyefloaters.console.api.BlockingSupplier.get;
 )
 public record NewTopic(
         @JsonProperty
-        @NotNull(payload = ErrorCategory.InvalidResource.class)
+        @NotBlank(payload = ErrorCategory.InvalidResource.class)
         String name,
 
         @JsonIgnore
