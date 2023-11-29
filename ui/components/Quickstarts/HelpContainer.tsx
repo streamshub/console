c2e650a1be46ad13ddc45aea82afa4d2d822a1d9
Riccardo Forina <rforina@redhat.com>
2023-11-29 11:08:54 +0100

    Perform the session refresh only after mounting the component
    Also, wrap the quickstart imports to catch SSR related errors

--- ui/components/Quickstarts/HelpContainer.tsx
+++ ui/components/Quickstarts/HelpContainer.tsx
@@ -1,17 +1,32 @@
 "use client";
-
-import { HelpTopicContext } from "@/libs/patternfly/quickstarts";
-import { HelpTopicContainer } from "@patternfly/quickstarts";
-import { PropsWithChildren, useContext } from "react";
+import dynamic from "next/dynamic";
+import React, { PropsWithChildren, useContext } from "react";
 import { helpTopics } from "./help-topics";
+
+let HelpTopicContext: any;
+try {
+  HelpTopicContext = require("@patternfly/quickstarts").HelpTopicContext;
+} catch {}
+
+const DynamicHelpTopicContainer = dynamic(
+  () => {
+    return import("@/libs/patternfly/quickstarts").then(
+      (mod) => mod.HelpTopicContainer,
+    );
+  },
+  { ssr: false },
+);
 
 export function HelpContainer({ children }: PropsWithChildren) {
   return (
-    <HelpTopicContainer helpTopics={helpTopics}>{children}</HelpTopicContainer>
+    <DynamicHelpTopicContainer helpTopics={helpTopics}>
+      {children}
+    </DynamicHelpTopicContainer>
   );
 }
 
 export function useHelp() {
+  // @ts-ignore
   const { setActiveHelpTopicByName } = useContext(HelpTopicContext);
   return function openHelp(content: "connect-to-cluster") {
     setActiveHelpTopicByName && setActiveHelpTopicByName(content);
