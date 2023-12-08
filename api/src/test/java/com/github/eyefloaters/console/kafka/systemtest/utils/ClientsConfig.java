801e9ab9373000807f82a3a162c8210f540f4294
Michael Edgar <medgar@redhat.com>
2023-12-08 11:48:15 -0500

    Remove "legacy" RHOSAK Java packages, tests, utilities; update configs (#238)
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- api/src/test/java/com/github/eyefloaters/console/kafka/systemtest/utils/ClientsConfig.java
+++ api/src/test/java/com/github/eyefloaters/console/kafka/systemtest/utils/ClientsConfig.java
@@ -1,5 +1,9 @@
 package com.github.eyefloaters.console.kafka.systemtest.utils;
 
+import java.util.Base64;
+import java.util.Properties;
+import java.util.UUID;
+
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -10,16 +14,10 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.eclipse.microprofile.config.Config;
 
-import com.github.eyefloaters.console.legacy.KafkaAdminConfigRetriever;
-
-import java.util.Base64;
-import java.util.Properties;
-import java.util.UUID;
-
 public class ClientsConfig {
     public static Properties getConsumerConfig(Config config, String groupID) {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(KafkaAdminConfigRetriever.BOOTSTRAP_SERVERS, String.class));
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.class));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupID == null ? UUID.randomUUID().toString() : groupID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -34,7 +32,7 @@ public class ClientsConfig {
 
     public static Properties getProducerConfig(Config config, String keySerializer, String valueSerializer) {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(KafkaAdminConfigRetriever.BOOTSTRAP_SERVERS, String.class));
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.class));
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
         props.put(ProducerConfig.ACKS_CONFIG, "all");
@@ -44,7 +42,7 @@ public class ClientsConfig {
     public static Properties getAdminConfigOauth(Config config, String token) {
         Properties props = new Properties();
 
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(KafkaAdminConfigRetriever.BOOTSTRAP_SERVERS, String.class));
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.class));
         props.put(AdminClientConfig.METADATA_MAX_AGE_CONFIG, "30000");
         props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "10000");
         props.put(AdminClientConfig.DEFAULT_API_TIMEOUT_MS_CONFIG, "30000");
@@ -56,7 +54,7 @@ public class ClientsConfig {
 
     public static Properties getAdminConfig(Config config) {
         Properties conf = new Properties();
-        conf.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(KafkaAdminConfigRetriever.BOOTSTRAP_SERVERS, String.class));
+        conf.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getValue(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.class));
         conf.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
         return conf;
     }
@@ -64,7 +62,7 @@ public class ClientsConfig {
     private static void addOauthConfig(Config config, Properties props, String token) {
         props.put(SaslConfigs.SASL_MECHANISM, "OAUTHBEARER");
 
-        config.getOptionalValue(KafkaAdminConfigRetriever.BROKER_TRUSTED_CERT, String.class)
+        config.getOptionalValue(SslConfigs.SSL_TRUSTSTORE_CERTIFICATES_CONFIG, String.class)
             // Value is base64 encoded to simulate configuration via Kubernetes env secret
             .map(Base64.getDecoder()::decode)
             .map(String::new)
