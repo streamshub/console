fe8b37ee7cfdcd4fc2d75aea5d272763422defae
Riccardo Forina <rforina@redhat.com>
2023-12-14 09:52:13 +0100

    Charts are by broker, show the ranges as stacked areas (#258)
    Also finalise the layout, add proper titles and tooltips.

--- ui/app/[locale]/kafka/[kafkaId]/overview/ClusterChartsCard.tsx
+++ ui/app/[locale]/kafka/[kafkaId]/overview/ClusterChartsCard.tsx
@@ -1,45 +1,99 @@
 "use client";
-import {
-  ChartDiskUsage,
-  TimeSeriesMetrics,
-} from "@/app/[locale]/kafka/[kafkaId]/overview/ChartDiskUsage";
+import { ChartCpuUsage } from "@/app/[locale]/kafka/[kafkaId]/overview/ChartCpuUsage";
+import { ChartDiskUsage } from "@/app/[locale]/kafka/[kafkaId]/overview/ChartDiskUsage";
+import { ChartMemoryUsage } from "@/app/[locale]/kafka/[kafkaId]/overview/ChartMemoryUsage";
 import { ChartSkeletonLoader } from "@/app/[locale]/kafka/[kafkaId]/overview/ChartSkeletonLoader";
 import {
   Card,
   CardBody,
   CardHeader,
   CardTitle,
+  Divider,
+  Flex,
 } from "@/libs/patternfly/react-core";
-import { useFormatBytes } from "@/utils/format";
+import { Title, Tooltip } from "@patternfly/react-core";
+import { HelpIcon } from "@patternfly/react-icons";
 
 type ClusterChartsCardProps = {
-  usedDiskSpace: TimeSeriesMetrics;
-  availableDiskSpace: TimeSeriesMetrics;
+  usedDiskSpace: TimeSeriesMetrics[];
+  availableDiskSpace: TimeSeriesMetrics[];
+  memoryUsage: TimeSeriesMetrics[];
+  cpuUsage: TimeSeriesMetrics[];
 };
 
 export function ClusterChartsCard({
   isLoading,
   usedDiskSpace,
   availableDiskSpace,
+  memoryUsage,
+  cpuUsage,
 }:
   | ({ isLoading: false } & ClusterChartsCardProps)
   | ({
       isLoading: true;
     } & Partial<{ [key in keyof ClusterChartsCardProps]?: undefined }>)) {
-  const formatter = useFormatBytes();
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Available disk space</CardTitle>
+        <CardTitle>
+          <Title headingLevel={"h2"} size={"lg"}>
+            Cluster metrics
+          </Title>
+        </CardTitle>
       </CardHeader>
       <CardBody>
-        {isLoading && <ChartSkeletonLoader height={300} padding={50} />}
-        {!isLoading && (
-          <ChartDiskUsage
-            usage={usedDiskSpace}
-            available={availableDiskSpace}
-          />
-        )}
+        <Flex direction={{ default: "column" }} gap={{ default: "gapLg" }}>
+          <b>
+            Available disk space{" "}
+            <Tooltip
+              content={
+                "Used and available disk capacity for all brokers over a specified period. Make sure there's enough space for everyday operations."
+              }
+            >
+              <HelpIcon />
+            </Tooltip>
+          </b>
+          {isLoading ? (
+            <ChartSkeletonLoader />
+          ) : (
+            <ChartDiskUsage
+              usages={usedDiskSpace}
+              available={availableDiskSpace}
+            />
+          )}
+          <Divider />
+          <b>
+            CPU usage{" "}
+            <Tooltip
+              content={
+                "CPU utilization for all brokers over a specified period. Sustained high usage may indicate the need for resource optimization."
+              }
+            >
+              <HelpIcon />
+            </Tooltip>
+          </b>
+          {isLoading ? (
+            <ChartSkeletonLoader />
+          ) : (
+            <ChartCpuUsage usages={cpuUsage} />
+          )}
+          <Divider />
+          <b>
+            Memory usage{" "}
+            <Tooltip
+              content={
+                "Memory utilization for all brokers over a specified period. Efficient memory allocation is essential for optimal performance of Kafka."
+              }
+            >
+              <HelpIcon />
+            </Tooltip>
+          </b>
+          {isLoading ? (
+            <ChartSkeletonLoader />
+          ) : (
+            <ChartMemoryUsage usages={memoryUsage} />
+          )}
+        </Flex>
       </CardBody>
     </Card>
   );
---
+++ ui/app/[locale]/kafka/[kafkaId]/overview/chartConsts.ts
@@ -0,0 +1,2 @@
+export const height = 200;
+export const padding = { bottom: 55, top: 5, left: 90, right: 20 };
