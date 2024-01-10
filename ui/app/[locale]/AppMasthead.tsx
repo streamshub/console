6dcac58d5df7cbe58b72b4204c1c0bdbbb5597f7
Riccardo Forina <rforina@redhat.com>
2024-01-10 10:45:03 +0100

    Add the @patternfly/react-user-feedback modal and link it to the help  icon in the masthead (#356)
    * Add the @patternfly/react-user-feedback modal and link it to the help icon in the masthead
    
    * Update support case url

--- ui/app/[locale]/AppMasthead.tsx
+++ ui/app/[locale]/AppMasthead.tsx
@@ -18,10 +18,12 @@ import {
   QuestionCircleIcon,
 } from "@/libs/patternfly/react-icons";
 import logo from "@/public/Logo-Red_Hat-AMQ-A-Reverse-RGB.svg";
+import { FeedbackModal } from "@patternfly/react-user-feedback";
 import { useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { UserDropdown } from "./UserDropdown";
 
 export function AppMasthead() {
@@ -29,71 +31,89 @@ export function AppMasthead() {
   const { data } = useSession();
   const { user } = data || {};
   const { toggleSidebar } = useAppLayout();
-
+  const [isFeedbackModalOpen, setIsFeedbackModalOpen] = useState(false);
+  const openFeedbackModal = () => {
+    setIsFeedbackModalOpen(true);
+  };
+  const closeFeedbackModal = () => {
+    setIsFeedbackModalOpen(false);
+  };
   return (
-    <Masthead>
-      <MastheadToggle>
-        <PageToggleButton
-          variant="plain"
-          aria-label="Global navigation"
-          onClick={toggleSidebar}
-        >
-          <BarsIcon />
-        </PageToggleButton>
-      </MastheadToggle>
-      <MastheadMain>
-        <Link href={"/"} className={"pf-v5-c-masthead_brand"}>
-          <Image
-            className={"pf-v5-c-brand"}
-            src={logo}
-            alt={t("common.title")}
-            priority={true}
-            style={{ height: 48 }}
-          />
-        </Link>
-      </MastheadMain>
-      <MastheadContent>
-        <Toolbar
-          ouiaId="masthead-toolbar"
-          id={"masthead-toolbar"}
-          isFullHeight
-          isStatic
-        >
-          <ToolbarContent id={"masthead-toolbar"}>
-            <ToolbarGroup
-              variant="icon-button-group"
-              align={{ default: "alignRight" }}
-              spacer={{ default: "spacerNone", md: "spacerMd" }}
-            >
+    <>
+      <Masthead>
+        <MastheadToggle>
+          <PageToggleButton
+            variant="plain"
+            aria-label="Global navigation"
+            onClick={toggleSidebar}
+          >
+            <BarsIcon />
+          </PageToggleButton>
+        </MastheadToggle>
+        <MastheadMain>
+          <Link href={"/"} className={"pf-v5-c-masthead_brand"}>
+            <Image
+              className={"pf-v5-c-brand"}
+              src={logo}
+              alt={t("common.title")}
+              priority={true}
+              style={{ height: 48 }}
+            />
+          </Link>
+        </MastheadMain>
+        <MastheadContent>
+          <Toolbar
+            ouiaId="masthead-toolbar"
+            id={"masthead-toolbar"}
+            isFullHeight
+            isStatic
+          >
+            <ToolbarContent id={"masthead-toolbar"}>
               <ToolbarGroup
                 variant="icon-button-group"
-                visibility={{ default: "hidden", lg: "visible" }}
+                align={{ default: "alignRight" }}
+                spacer={{ default: "spacerNone", md: "spacerMd" }}
               >
-                <ToolbarItem>
-                  <Button
-                    aria-label="Settings"
-                    variant={"plain"}
-                    icon={<CogIcon />}
-                    ouiaId={"setting-button"}
-                  />
-                </ToolbarItem>
-                <ToolbarItem>
-                  <Button
-                    aria-label="Help"
-                    variant={"plain"}
-                    icon={<QuestionCircleIcon />}
-                    ouiaId={"help-button"}
-                  />
-                </ToolbarItem>
+                <ToolbarGroup
+                  variant="icon-button-group"
+                  visibility={{ default: "hidden", lg: "visible" }}
+                >
+                  <ToolbarItem>
+                    <Button
+                      aria-label="Settings"
+                      variant={"plain"}
+                      icon={<CogIcon />}
+                      ouiaId={"setting-button"}
+                    />
+                  </ToolbarItem>
+                  <ToolbarItem>
+                    <Button
+                      aria-label="Help"
+                      variant={"plain"}
+                      icon={<QuestionCircleIcon />}
+                      ouiaId={"help-button"}
+                      onClick={openFeedbackModal}
+                    />
+                  </ToolbarItem>
+                </ToolbarGroup>
               </ToolbarGroup>
-            </ToolbarGroup>
-            <UserDropdown
-              username={user?.name || user?.email}
-              picture={user?.picture}
-            />
-          </ToolbarContent>
-        </Toolbar>
-      </MastheadContent>
-    </Masthead>
+              <UserDropdown
+                username={user?.name || user?.email}
+                picture={user?.picture}
+              />
+            </ToolbarContent>
+          </Toolbar>
+        </MastheadContent>
+      </Masthead>
+      <FeedbackModal
+        onShareFeedback="https://github.com/eyefloaters/console/issues/new?assignees=&labels=&projects=&template=share-feeedback.md&title="
+        onJoinMailingList="https://github.com/eyefloaters/console/issues/new?assignees=&labels=&projects=&template=inform-the-direction-of-products.md&title="
+        onOpenSupportCase="https://access.redhat.com/support/cases/#/case/new/get-support?caseCreate=true"
+        onReportABug="https://github.com/eyefloaters/console/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
+        feedbackImg={"/pf_feedback.svg"}
+        isOpen={isFeedbackModalOpen}
+        onClose={closeFeedbackModal}
+      />
+    </>
   );
 }
