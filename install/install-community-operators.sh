4f12a31cba1fc15a3901534ba7db59c1451b041c
Michael Edgar <medgar@redhat.com>
2024-02-01 16:47:22 -0500

    Basic script to install community operator dependencies
    Signed-off-by: Michael Edgar <medgar@redhat.com>

---
+++ install/install-community-operators.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export NAMESPACE="${1}"
+
+echo "apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: strimzi-kafka-operator
+spec:
+  name: strimzi-kafka-operator
+  channel: stable
+  source: community-operators
+  sourceNamespace: openshift-marketplace" \
+ | kubectl apply -n ${NAMESPACE} -f -
+
+echo "apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: keycloak-operator
+spec:
+  name: keycloak-operator
+  channel: fast
+  source: community-operators
+  sourceNamespace: openshift-marketplace" \
+ | kubectl apply -n ${NAMESPACE} -f -
+
+echo "apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: prometheus-operator
+spec:
+  name: prometheus
+  channel: beta
+  source: community-operators
+  sourceNamespace: openshift-marketplace" \
+ | kubectl apply -n ${NAMESPACE} -f -
