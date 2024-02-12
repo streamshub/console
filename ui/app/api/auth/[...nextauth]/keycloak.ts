888a76f8d1a8b027391a5f6d30abaad0f3d52590
Michael Edgar <medgar@redhat.com>
2024-02-12 07:25:10 -0500

    Experimental: Add support for OCP OAuth authentication
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/app/api/auth/[...nextauth]/keycloak.ts
+++ ui/app/api/auth/[...nextauth]/keycloak.ts
@@ -5,45 +5,55 @@ import { logger } from "@/utils/logger";
 
 const log = logger.child({ module: "keycloak" });
 
-export const keycloak = KeycloakProvider({
-  clientId: process.env.KEYCLOAK_CLIENTID,
-  clientSecret: process.env.KEYCLOAK_CLIENTSECRET,
-  issuer: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
-});
+class Keycloak {
+  provider = KeycloakProvider({
+    clientId: process.env.CONSOLE_OAUTH_CLIENT_ID,
+    clientSecret: process.env.CONSOLE_OAUTH_CLIENT_SECRET,
+    issuer: process.env.CONSOLE_OAUTH_ISSUER,
+  });
 
-let _tokenEndpoint: string | undefined = undefined;
-async function getTokenEndpoint() {
-  if (keycloak.wellKnown) {
-    const kc = await fetch(keycloak.wellKnown);
-    const res = await kc.json();
-    _tokenEndpoint = res.token_endpoint;
+  isEnabled() {
+    return process.env.CONSOLE_OAUTH_PROVIDER === "keycloak"
+      && this.provider.options?.issuer
+      && this.provider.options?.clientId
+      && this.provider.options?.clientSecret;
   }
-  return _tokenEndpoint;
-}
 
-export async function refreshToken(token: JWT): Promise<JWT> {
-  try {
-    const tokenEndpoint = await getTokenEndpoint();
-    if (!tokenEndpoint) {
-      log.error("Invalid Keycloak wellKnow");
-      throw token;
+  async getTokenEndpoint() {
+    let _tokenEndpoint: string | undefined = undefined;
+
+    if (this.provider.wellKnown) {
+      const kc = await fetch(this.provider.wellKnown);
+      const res = await kc.json();
+      _tokenEndpoint = res.token_endpoint;
     }
-    let tokenExpiration = new Date(
-      (typeof token?.expires_at === "number" ? token.expires_at : 0) * 1000,
-    );
-    log.trace({ tokenExpiration }, "Token expiration");
 
-    if (Date.now() < tokenExpiration.getTime()) {
-      log.trace(token, "Token not yet expired");
-      return token;
-    } else {
+    return _tokenEndpoint;
+  }
+
+  async refreshToken(token: JWT): Promise<JWT> {
+    try {
+      const tokenEndpoint = await this.getTokenEndpoint();
+      if (!tokenEndpoint) {
+        log.error("Invalid Keycloak wellKnow");
+        throw token;
+      }
+      let tokenExpiration = new Date(
+        (typeof token?.expires_at === "number" ? token.expires_at : 0) * 1000,
+      );
+      log.trace({ tokenExpiration }, "Token expiration");
+
+      if (Date.now() < tokenExpiration.getTime()) {
+        log.trace(token, "Token not yet expired");
+        return token;
+      }
       log.trace(token, "Token has expired");
       let refresh_token =
         typeof token.refresh_token === "string" ? token.refresh_token : "";
 
       const params = {
-        client_id: keycloak.options!.clientId,
-        client_secret: keycloak.options!.clientSecret,
+        client_id: this.provider.options!.clientId,
+        client_secret: this.provider.options!.clientSecret,
         grant_type: "refresh_token",
         refresh_token: refresh_token,
       };
@@ -82,16 +92,48 @@ export async function refreshToken(token: JWT): Promise<JWT> {
       };
       log.trace(newToken, "New token");
       return newToken;
+    } catch (error: unknown) {
+      if (typeof error === "string") {
+        log.error({ message: error }, "Error refreshing access token");
+      } else if (error instanceof Error) {
+        log.error(error, "Error refreshing access token");
+      } else {
+        log.error("Unknown error refreshing access token");
+      }
+      // The error property will be used client-side to handle the refresh token error
+      return { ...token, error: "RefreshAccessTokenError" as const };
     }
-  } catch (error: unknown) {
-    if (typeof error === "string") {
-      log.error({ message: error }, "Error refreshing access token");
-    } else if (error instanceof Error) {
-      log.error(error, "Error refreshing access token");
-    } else {
-      log.error("Unknown error refreshing access token");
-    }
-    // The error property will be used client-side to handle the refresh token error
-    return { ...token, error: "RefreshAccessTokenError" as const };
   }
+
+  async jwt({ token, account }: { token: JWT, account: any }) {
+    // Persist the OAuth access_token and or the user id to the token right after signin
+    if (account) {
+      log.trace("account present, saving new token");
+      // Save the access token and refresh token in the JWT on the initial login
+      return {
+        access_token: account.access_token,
+        expires_at: account.expires_at,
+        refresh_token: account.refresh_token,
+        email: token.email,
+        name: token.name,
+        picture: token.picture,
+        sub: token.sub,
+      };
+    }
+
+    return this.refreshToken(token);
+  };
+
+  async session({ session, token }: { session: Session, token: JWT }) {
+    // Send properties to the client, like an access_token from a provider.
+    log.trace(token, "Creating session from token");
+    return {
+      ...session,
+      error: token.error,
+      accessToken: token.access_token,
+    };
+  };
 }
+
+const keycloak = new Keycloak();
+export default keycloak;
