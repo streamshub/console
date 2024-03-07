2a5827a7ea21408ca3ead883649d27e060c566c0
Riccardo Forina <rforina@redhat.com>
2024-03-07 09:35:43 +0100

    PartitionSelector stories

---
+++ ui/components/MessagesTable/components/PartitionSelector.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { expect, fn, within } from "@storybook/test";
+import { PartitionSelector as Comp } from "./PartitionSelector";
+
+const meta: Meta<typeof Comp> = {
+  component: Comp,
+  args: {
+    onChange: fn(),
+    partitions: 2,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Comp>;
+
+export const DefaultValue: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("All partitions"));
+  },
+};
+
+export const WithInitialValue: Story = {
+  args: {
+    value: 1,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Partition 1"));
+  },
+};
