2f6a7304f9c62babf6301c9c9b0f2cae0865cd72
Riccardo Forina <rforina@redhat.com>
2024-03-07 15:48:34 +0100

    Add a Tech Preview banner and relative UI elements (#563)

--- ui/app/[locale]/AppLayout.tsx
+++ ui/app/[locale]/AppLayout.tsx
@@ -6,7 +6,16 @@ import { ClusterDrawer } from "@/app/[locale]/ClusterDrawer";
 import { ClusterDrawerProvider } from "@/app/[locale]/ClusterDrawerProvider";
 import { NavExpandable } from "@/components/NavExpandable";
 import { NavItemLink } from "@/components/NavItemLink";
-import { Nav, NavList, Page } from "@/libs/patternfly/react-core";
+import { TechPreviewPopover } from "@/components/TechPreviewPopover";
+import {
+  Banner,
+  Nav,
+  NavList,
+  Page,
+  Split,
+  SplitItem,
+} from "@/libs/patternfly/react-core";
+import { HelpIcon } from "@/libs/patternfly/react-icons";
 import { PropsWithChildren, Suspense } from "react";
 
 export function AppLayout({ children }: PropsWithChildren) {
@@ -37,7 +46,22 @@ export function AppLayout({ children }: PropsWithChildren) {
     >
       {/*<HelpContainer>*/}
       <ClusterDrawerProvider>
-        <ClusterDrawer>{children}</ClusterDrawer>
+        <ClusterDrawer>
+          <Banner variant={"blue"}>
+            <Split>
+              <SplitItem isFilled={true}>
+                This is a Tech Preview version. You are logged in as an
+                anonymous user with read-only access.
+              </SplitItem>
+              <SplitItem>
+                <TechPreviewPopover>
+                  <HelpIcon />
+                </TechPreviewPopover>
+              </SplitItem>
+            </Split>
+          </Banner>
+          {children}
+        </ClusterDrawer>
       </ClusterDrawerProvider>
       {/*</HelpContainer>*/}
     </Page>
@@ -46,26 +70,28 @@ export function AppLayout({ children }: PropsWithChildren) {
 
 async function Clusters() {
   const clusters = await getKafkaClusters();
-  return clusters.filter(c => c.meta.configured === true).map((s, idx) => (
-    <NavExpandable
-      key={s.id}
-      title={s.attributes.name}
-      url={`/kafka/${s.id}`}
-      startExpanded={idx === 0}
-    >
-      <NavItemLink url={`/kafka/${s.id}/overview`}>
-        Cluster overview
-      </NavItemLink>
-      <NavItemLink url={`/kafka/${s.id}/topics`}>Topics</NavItemLink>
-      <NavItemLink url={`/kafka/${s.id}/nodes`}>Brokers</NavItemLink>
-      {/*
+  return clusters
+    .filter((c) => c.meta.configured === true)
+    .map((s, idx) => (
+      <NavExpandable
+        key={s.id}
+        title={s.attributes.name}
+        url={`/kafka/${s.id}`}
+        startExpanded={idx === 0}
+      >
+        <NavItemLink url={`/kafka/${s.id}/overview`}>
+          Cluster overview
+        </NavItemLink>
+        <NavItemLink url={`/kafka/${s.id}/topics`}>Topics</NavItemLink>
+        <NavItemLink url={`/kafka/${s.id}/nodes`}>Brokers</NavItemLink>
+        {/*
       <NavItemLink url={`/kafka/${s.id}/service-registry`}>
         Service registry
       </NavItemLink>
 */}
-      <NavItemLink url={`/kafka/${s.id}/consumer-groups`}>
-        Consumer groups
-      </NavItemLink>
-    </NavExpandable>
-  ));
+        <NavItemLink url={`/kafka/${s.id}/consumer-groups`}>
+          Consumer groups
+        </NavItemLink>
+      </NavExpandable>
+    ));
 }
---
+++ ui/components/TechPreviewPopover.tsx
@@ -0,0 +1,35 @@
+import { ExternalLink } from "@/components/ExternalLink";
+import { Popover } from "@/libs/patternfly/react-core";
+import { PopoverProps } from "@patternfly/react-core";
+
+export function TechPreviewPopover({
+  children,
+}: {
+  children: PopoverProps["children"];
+}) {
+  return (
+    <Popover
+      triggerAction={"hover"}
+      headerContent={"Technology Preview"}
+      bodyContent={
+        <div>
+          Technology Preview features are not fully supported, may not be
+          functionally complete, and are not suitable for deployment in
+          production. However, these features are provided to the customer as a
+          courtesy and the primary goal is for the feature to gain wider
+          exposure with the goal of full support in the future.
+        </div>
+      }
+      footerContent={
+        <ExternalLink
+          href={"https://redhat.com"}
+          testId={"tech-preview-learn-more"}
+        >
+          Learn more
+        </ExternalLink>
+      }
+    >
+      {children}
+    </Popover>
+  );
+}
