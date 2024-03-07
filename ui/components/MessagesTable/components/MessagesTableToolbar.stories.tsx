c07fd01a49a2499fffce7b515f76d61547b7f918
Riccardo Forina <rforina@redhat.com>
2024-03-07 08:55:32 +0100

    Flatten the story folder for MessagesTableToolbar

--- ui/components/MessagesTable/components/MessagesTableToolbar.stories.tsx
+++ ui/components/MessagesTable/components/MessagesTableToolbar.stories.tsx
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { expect, fn, userEvent, within } from "@storybook/test";
-import { MessagesTableToolbar } from "./MessagesTableToolbar";
+import { MessagesTableToolbar as Comp } from "./MessagesTableToolbar";
 
-const meta: Meta<typeof MessagesTableToolbar> = {
-  component: MessagesTableToolbar,
+const meta: Meta<typeof Comp> = {
+  component: Comp,
   args: {
     onColumnManagement: fn(),
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof MessagesTableToolbar>;
+type Story = StoryObj<typeof Comp>;
 
-export const Default: Story = {
+export const MessagesTableToolbar: Story = {
   play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
     // Assuming the button to manage columns can be identified by its aria-label or text
