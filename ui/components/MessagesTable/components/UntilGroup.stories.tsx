816e2aa8f9e217c2377acf0d18b810b368cb9bc7
Riccardo Forina <rforina@redhat.com>
2024-03-08 12:53:11 +0100

    Fix the Retrieve dropdown; add stories

---
+++ ui/components/MessagesTable/components/UntilGroup.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { expect, fn, within } from "@storybook/test";
+import { UntilGroup as Comp } from "./UntilGroup";
+
+const meta: Meta<typeof Comp> = {
+  component: Comp,
+  args: {
+    onLimitChange: fn(),
+    onLive: fn(),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Comp>;
+
+export const Limit: Story = {
+  args: {
+    limit: 50,
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(args.limit, { exact: false }));
+  },
+};
+
+export const Continuosly: Story = {
+  args: {
+    limit: "continuously",
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.findByText("Continuously"));
+  },
+};
