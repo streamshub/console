c62c4f569fad6082e1c5d878ba41cc4b15652b48
hemahg <hhg@redhat.com>
2024-05-27 23:05:19 +0530

    add text to translation file

--- ui/app/[locale]/kafka/[kafkaId]/nodes/DistributionChart.tsx
+++ ui/app/[locale]/kafka/[kafkaId]/nodes/DistributionChart.tsx
@@ -144,8 +144,7 @@ export function DistributionChart({
                 data={Object.keys(data).map((node) => {
                   const count = getCount(data[node]);
                   const percentage = getPercentage(count);
-                  const name = `Broker ${node}: ${count} partitions (${percentage}%)`;
-                  return { name };
+                  return { name: t("DistributionChart.broker_node_count", { node, count, percentage }) };
                 })}
                 itemsPerRow={width > 600 ? 3 : 1}
               />
