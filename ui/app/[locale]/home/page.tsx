33524bd34e9f2c225bc4a0f04444e546f0aabbb3
Hema HG <53568062+hemahg@users.noreply.github.com>
2024-06-13 18:14:02 +0530

    fix typo (#821)
    * fix typo
    
    Signed-off-by: hemahg <hhg@redhat.com>
    
    * Add text to translation file
    
    Signed-off-by: hemahg <hhg@redhat.com>
    
    ---------
    
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/app/[locale]/home/page.tsx
+++ ui/app/[locale]/home/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
               isCompact={true}
             >
               <CardBody>
-                <DataList aria-label="Reccomended learning resources">
+                <DataList aria-label={t("homepage.recommended_learning_resources_label")}>
                   <DataListItem aria-labelledby="gs-1-1">
                     <DataListItemRow>
                       <DataListItemCells
