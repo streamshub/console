226b058a8e66feb465da3f79188e8dc9874662fa
jkalinic <jkalinic@redhat.com>
2024-07-15 21:12:40 +0200

    Fix operator UI on openshift and add new annotation fields
    Signed-off-by: jkalinic <jkalinic@redhat.com>

---
+++ operator/bin/modify-bundle-metadata.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+source ${SCRIPT_PATH}/common.sh
+
+ARCH=linux/amd64
+SKIP_RANGE=""
+OPERATOR_NAME="console-operator"
+
+for ARGUMENT in "$@"
+do
+  KEY=$(echo "$ARGUMENT" | sed 's/=\(.*\)//')
+  VALUE=$(echo "$ARGUMENT" | sed 's/^[^=]*=//')
+
+  case "$KEY" in
+    ARCH)     			                        ARCH=${VALUE} ;;
+    SKIP_RANGE)                            SKIP_RANGE=${VALUE};;
+    OPERATOR_NAME)                      OPERATOR_NAME=${VALUE};;
+    *)
+  esac
+done
+
+echo "[INFO] Modify values and replace placeholders in ${CSV_FILE_PATH}"
+
+yq_image_expression=".spec.install.spec.deployments[0] | (select (.name ==\"${OPERATOR_NAME}\")).spec.template.spec.containers[].image"
+
+full_image=$(${YQ} eval "${yq_image_expression}" "${CSV_FILE_PATH}")
+echo "[DEBUG] Original image name = ${full_image}"
+
+image_name=$(echo "${full_image}" | cut -d':' -f1)
+digest=$(${SKOPEO} inspect --tls-verify=false --override-os=linux --format "{{ .Digest }}" "docker://${full_image}")
+image_with_digest="${image_name}@${digest}"
+
+# Create relatedImages section
+${YQ} eval -o yaml -i ".spec.relatedImages = null" "${CSV_FILE_PATH}"
+
+# Add operator image
+${YQ} eval -o yaml -i ".spec.relatedImages += [{\"name\": \"${OPERATOR_NAME}\", \"image\": \"${image_with_digest}\"}]" "${CSV_FILE_PATH}";
+echo "[DEBUG] Setting container image = ${image_with_digest}"
+${YQ} eval -o yaml -i ".metadata.annotations.containerImage = \"${image_with_digest}\"" "${CSV_FILE_PATH}"
+
+# Add current createdAt time
+curr_time_date=$(date +'%Y-%m-%d %H:%M:%S')
+echo "[DEBUG] Setting createdAt = ${curr_time_date}"
+${YQ} eval -o yaml -i ".metadata.annotations.createdAt = \"${curr_time_date}\"" "${CSV_FILE_PATH}"
+
+# Add skipRange if present
+if [[ -n "$SKIP_RANGE" ]]; then
+    echo "[DEBUG] Setting skipRange = \"${SKIP_RANGE}\""
+    ${YQ} eval -o yaml -i ".metadata.annotations.[\"olm.skipRange\"] = \"${SKIP_RANGE}\"" "${CSV_FILE_PATH}"
+fi
--- operator/src/main/java/com/github/streamshub/console/ConsoleReconciler.java
+++ operator/src/main/java/com/github/streamshub/console/ConsoleReconciler.java
@@ -40,6 +40,8 @@ import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
+import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Annotations;
+import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Annotations.Annotation;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata.InstallMode;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Provider;
 
@@ -117,6 +119,20 @@ import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Provider;
         })
 @CSVMetadata(
         provider = @Provider(name = "StreamsHub", url = "https://github.com/streamshub"),
+        annotations = @Annotations(
+            containerImage = "placeholder",
+            repository = "https://github.com/streamshub/console",
+            capabilities = "Basic Install",
+            categories = "Streaming & Messaging",
+            certified = true,
+            skipRange = "placeholder",
+            others = {
+                @Annotation(name = "features.operators.openshift.io/fips-compliant", value = "true"),
+                @Annotation(name = "features.operators.openshift.io/disconnected", value = "true"),
+                @Annotation(name = "features.operators.openshift.io/proxy-aware", value = "true"),
+                @Annotation(name = "createdAt", value = "placeholder"),
+                @Annotation(name = "support", value = "Streamshub")
+            }),
         description = "StreamsHub console provides a user interface for managing and monitoring your streaming resources",
         installModes = {
             @InstallMode(type = "AllNamespaces", supported = true),
