ffc56911b8ca8bac98d71a5d40abf6f70a867de1
Michael Edgar <medgar@redhat.com>
2024-09-06 07:21:22 -0400

    Apicurio Registry integration with Avro+Protobuf ser/des support
    Signed-off-by: Michael Edgar <medgar@redhat.com>

---
+++ api/src/main/java/com/github/streamshub/console/api/SchemasResource.java
@@ -0,0 +1,93 @@
+package com.github.streamshub.console.api;
+
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.streamshub.console.api.support.KafkaContext;
+import com.github.streamshub.console.api.support.serdes.ArtifactReferences;
+import com.github.streamshub.console.api.support.serdes.MultiformatSchemaParser;
+
+import io.apicurio.registry.resolver.DefaultSchemaResolver;
+import io.apicurio.registry.resolver.SchemaResolver;
+import io.apicurio.registry.rest.client.RegistryClient;
+
+@Path("/api/schemas/{schemaId}")
+@Tag(name = "Schema Registry Resources")
+public class SchemasResource {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    @Inject
+    Map<String, KafkaContext> kafkaContexts;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponse(responseCode = "200", ref = "Configurations", content = @Content())
+    @APIResponse(responseCode = "404", ref = "NotFound")
+    @APIResponse(responseCode = "500", ref = "ServerError")
+    @APIResponse(responseCode = "504", ref = "ServerTimeout")
+    public Response describeConfigs(
+            @Parameter(description = "Schema identifier")
+            @PathParam("schemaId")
+            String schemaId) {
+
+        int clusterTerm = schemaId.indexOf('.');
+
+        if (clusterTerm < 0) {
+            throw new NotFoundException("No such schema");
+        }
+
+        String clusterId = new String(Base64.getUrlDecoder().decode(schemaId.substring(0, clusterTerm)));
+
+        if (!kafkaContexts.containsKey(clusterId)) {
+            throw new NotFoundException("No such schema");
+        }
+
+        RegistryClient registryClient = kafkaContexts.get(clusterId).schemaRegistryContext().registryClient();
+
+        if (registryClient == null) {
+            throw new NotFoundException("Schema not found, no registry is configured");
+        }
+
+        @SuppressWarnings("resource")
+        SchemaResolver<Object, ?> schemaResolver = new DefaultSchemaResolver<>();
+        schemaResolver.setClient(registryClient);
+        schemaResolver.configure(Collections.emptyMap(), new MultiformatSchemaParser<>(Collections.emptySet()));
+
+        schemaId = schemaId.substring(clusterTerm + 1);
+
+        var reference = ArtifactReferences.fromSchemaId(schemaId, objectMapper);
+        var schema = schemaResolver.resolveSchemaByArtifactReference(reference);
+
+        var response = Optional.ofNullable(schema)
+                .map(s -> s.getParsedSchema())
+                .map(s -> s.getRawSchema())
+                .map(Response::ok)
+                .orElseThrow(() -> new NotFoundException("No such schema"));
+
+        return response.build();
+    }
+
+}
