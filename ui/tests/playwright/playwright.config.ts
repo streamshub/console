2b8c77a37c84d13254695f863e98342200661780
Riccardo Forina <rforina@redhat.com>
2024-09-24 10:33:15 +0200

    Use a default address when running tests without a base url

--- ui/tests/playwright/playwright.config.ts
+++ ui/tests/playwright/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     },
   ],
   use: {
-    baseURL: process.env.PLAYWRIGHT_BASE_URL,
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:3000",
     ignoreHTTPSErrors: true,
   },
 });
