86ff8ae76679f34bc08ae1bfd676aa8f01ad10af
hemahg <hhg@redhat.com>
2024-11-19 19:27:22 +0530

    use Tab to switch between the tab
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/components/Navigation/NavTabLink.tsx
+++ ui/components/Navigation/NavTabLink.tsx
@@ -5,7 +5,7 @@ import {
   TabTitleText,
   TabsComponent,
 } from "@/libs/patternfly/react-core";
-import { usePathname } from "@/i18n/routing";
+import { usePathname, useRouter } from "@/i18n/routing";
 import { Route } from "next";
 
 type TabData<T extends string> = {
@@ -16,10 +16,17 @@ type TabData<T extends string> = {
 
 export function NavTabLink<T extends string>({ tabs }: { tabs: TabData<T>[] }) {
   const pathname = usePathname();
+  const router = useRouter();
+
   const activeTabKey =
     tabs.find((tab) => pathname === tab.url.toString())?.key ??
     tabs.find((tab) => pathname.startsWith(tab.url.toString()))?.key ??
     0;
+
+  const handleTabClick = (url: Route<T> | URL) => (event: React.MouseEvent) => {
+    event.preventDefault(); // Prevent default link navigation
+    router.push(url.toString()); // Trigger Next.js navigation
+  };
 
   return (
     <Tabs
@@ -33,6 +40,7 @@ export function NavTabLink<T extends string>({ tabs }: { tabs: TabData<T>[] }) {
           eventKey={key}
           title={<TabTitleText>{title}</TabTitleText>}
           href={url.toString()}
+          onClick={handleTabClick(url)}
         />
       ))}
     </Tabs>
