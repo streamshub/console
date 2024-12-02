484b73aae0a0eb02bf9cc3adf621976473ca91ec
Jan Kalinic <jkalinic@redhat.com>
2024-12-03 00:45:57 +0100

    Change image tags with digests in bundle metadata (#1246)
    * [WIP] Change tags with digests in bundle metadata
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Test workflow integration
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Test workflow integration 2
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Test workflow integration changed cut
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Test workflow integration registry added
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Test workflow integration operator build added
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] also build dependent projects
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] setup java17
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] add images
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] rework integration.yaml into one build job
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] fix pw dependent job
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] fix images archive name
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Modify release in the same way as integration
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Modify platforms env
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * [WIP] Modify snapshot workflow
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * Use SHA digest for operator deployment image within CSV
    
    Signed-off-by: Michael Edgar <medgar@redhat.com>
    
    * Use `skopeo sync` to export/import images, use ubuntu-24.04 runners
    
    Signed-off-by: Michael Edgar <medgar@redhat.com>
    
    ---------
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    Signed-off-by: Michael Edgar <medgar@redhat.com>
    Co-authored-by: Michael Edgar <medgar@redhat.com>

--- operator/bin/common.sh
+++ operator/bin/common.sh
@@ -5,6 +5,7 @@ SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 BUNDLE_PATH=${SCRIPT_PATH}/../target/bundle/console-operator/
 CSV_FILE_PATH=${BUNDLE_PATH}/manifests/console-operator.clusterserviceversion.yaml
 CATALOG_PATH=${SCRIPT_PATH}/../target/catalog
+OPERATOR_CATALOG_CONFIG_YAML_PATH=${CATALOG_PATH}/operator.yaml
 # Operator naming
 ORIGINAL_OPERATOR_NAME="console-operator"
 OPERATOR_NAME="streamshub-console-operator"
