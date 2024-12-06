b6c96d8be419b174221fa8f46c93c7d5b83f65e7
hemahg <hhg@redhat.com>
2024-12-06 11:56:41 +0530

    fix lint warnings
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/app/[locale]/(authorized)/SessionRefresher.tsx
+++ ui/app/[locale]/(authorized)/SessionRefresher.tsx
@@ -2,30 +2,29 @@
 import { logger } from "@/utils/loggerClient";
 import { useInterval } from "@patternfly/react-core";
 import { signIn, useSession } from "next-auth/react";
-import { useLayoutEffect } from "react";
+import { useCallback, useLayoutEffect } from "react";
 
 const log = logger.child({ module: "UI", component: "SessionRefresher" });
 
 export function SessionRefresher() {
   const { update } = useSession();
 
-  async function doRefresh() {
+  const doRefresh = useCallback(async () => {
     const refreshedToken = await update();
     log.debug(refreshedToken, "Refreshed session");
     if (refreshedToken?.error === "RefreshAccessTokenError") {
       log.debug("Force sign in");
-      void signIn(); // Force sign in to hopefully resolve error
+      void signIn();
     }
-  }
+  }, [update]);
 
   useInterval(() => {
     void doRefresh();
   }, 1000 * 60);
 
-  // immediately trigger a check to deal with expired sessions when opening the app after a long time
   useLayoutEffect(() => {
     void doRefresh();
-  }, []);
+  }, [doRefresh]);
 
   return null;
 }
