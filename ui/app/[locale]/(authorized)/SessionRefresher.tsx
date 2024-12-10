17a86036bb7eda8e5b34f3445181e36b960856fa
hemahg <hhg@redhat.com>
2024-12-10 14:57:51 +0530

    debug test
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/app/[locale]/(authorized)/SessionRefresher.tsx
+++ ui/app/[locale]/(authorized)/SessionRefresher.tsx
@@ -2,29 +2,30 @@
 import { logger } from "@/utils/loggerClient";
 import { useInterval } from "@patternfly/react-core";
 import { signIn, useSession } from "next-auth/react";
-import { useCallback, useLayoutEffect } from "react";
+import { useLayoutEffect } from "react";
 
 const log = logger.child({ module: "UI", component: "SessionRefresher" });
 
 export function SessionRefresher() {
   const { update } = useSession();
 
-  const doRefresh = useCallback(async () => {
+  async function doRefresh() {
     const refreshedToken = await update();
     log.debug(refreshedToken, "Refreshed session");
     if (refreshedToken?.error === "RefreshAccessTokenError") {
       log.debug("Force sign in");
-      void signIn();
+      void signIn(); // Force sign in to hopefully resolve error
     }
-  }, [update]);
+  }
 
   useInterval(() => {
     void doRefresh();
   }, 1000 * 60);
 
+  // immediately trigger a check to deal with expired sessions when opening the app after a long time
   useLayoutEffect(() => {
     void doRefresh();
-  }, [doRefresh]);
+  }, []);
 
   return null;
 }
