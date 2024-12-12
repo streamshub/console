8cf63b1347a69dc18b70a7d481e6cb028a2bb482
Michael Edgar <medgar@redhat.com>
2024-12-12 16:15:10 -0500

    [WIP] File-based OLM catalog with history
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- operator/bin/common.sh
+++ operator/bin/common.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-
-BUNDLE_PATH=${SCRIPT_PATH}/../target/bundle/streamshub-console-operator/
-CSV_FILE_PATH=${BUNDLE_PATH}/manifests/streamshub-console-operator.clusterserviceversion.yaml
-CATALOG_PATH=${SCRIPT_PATH}/../target/catalog
-OPERATOR_CATALOG_CONFIG_YAML_PATH=${CATALOG_PATH}/operator.yaml
-# Operator naming
-ORIGINAL_OPERATOR_NAME="console-operator"
-OPERATOR_NAME="streamshub-console-operator"
-OPERATOR_INSTANCE_NAME="${OPERATOR_NAME}-v${VERSION}"
-OPERATOR_CSV_NAME="${OPERATOR_NAME}.v${VERSION}"
-
 YQ="$(which yq 2>/dev/null)" || :
 
 if [ "${YQ}" == "" ] ; then
