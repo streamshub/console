ee6cf448706b5dac089fa2863b13d9e44c6632db
Michael Edgar <medgar@redhat.com>
2024-12-18 12:02:54 -0500

    add security testing, minor re-factoring
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- operator/src/test/java/com/github/streamshub/console/ConsoleReconcilerTest.java
+++ operator/src/test/java/com/github/streamshub/console/ConsoleReconcilerTest.java
@@ -1,26 +1,15 @@
 package com.github.streamshub.console;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import jakarta.inject.Inject;
-
 import org.apache.kafka.common.config.SaslConfigs;
-import org.eclipse.microprofile.config.Config;
-import org.jboss.logging.Logger;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.streamshub.console.api.v1alpha1.Console;
 import com.github.streamshub.console.api.v1alpha1.ConsoleBuilder;
 import com.github.streamshub.console.api.v1alpha1.spec.TrustStore;
@@ -29,184 +18,40 @@ import com.github.streamshub.console.api.v1alpha1.spec.metrics.MetricsSource.Typ
 import com.github.streamshub.console.api.v1alpha1.status.Condition;
 import com.github.streamshub.console.config.ConsoleConfig;
 import com.github.streamshub.console.config.PrometheusConfig;
+import com.github.streamshub.console.dependents.ConsoleDeployment;
 import com.github.streamshub.console.dependents.ConsoleResource;
 import com.github.streamshub.console.dependents.ConsoleSecret;
+import com.github.streamshub.console.dependents.PrometheusDeployment;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.EnvVar;
-import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
 import io.fabric8.kubernetes.api.model.Volume;
 import io.fabric8.kubernetes.api.model.VolumeMount;
-import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionBuilder;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.openshift.api.model.Route;
 import io.fabric8.openshift.api.model.RouteBuilder;
-import io.javaoperatorsdk.operator.Operator;
 import io.quarkus.test.junit.QuarkusTest;
-import io.strimzi.api.kafka.Crds;
-import io.strimzi.api.kafka.model.kafka.Kafka;
-import io.strimzi.api.kafka.model.kafka.KafkaBuilder;
-import io.strimzi.api.kafka.model.kafka.listener.KafkaListenerAuthenticationScramSha512;
-import io.strimzi.api.kafka.model.kafka.listener.KafkaListenerType;
 import io.strimzi.api.kafka.model.user.KafkaUser;
 import io.strimzi.api.kafka.model.user.KafkaUserBuilder;
 import io.strimzi.api.kafka.model.user.KafkaUserScramSha512ClientAuthentication;
 
 import static org.awaitility.Awaitility.await;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
-class ConsoleReconcilerTest {
-
-    private static final Logger LOGGER = Logger.getLogger(ConsoleReconcilerTest.class);
-    private static final Duration LIMIT = Duration.ofSeconds(10);
-    private static final ObjectMapper YAML = new ObjectMapper(new YAMLFactory());
-
-    @Inject
-    KubernetesClient client;
-
-    @Inject
-    Config config;
-
-    @Inject
-    Operator operator;
-
-    Kafka kafkaCR;
-
-    public static <T extends HasMetadata> T apply(KubernetesClient client, T resource) {
-        client.resource(resource).serverSideApply();
-        return client.resource(resource).patchStatus();
-    }
-
-    @BeforeEach
-    void setUp() throws Exception {
-        client.resource(Crds.kafka()).serverSideApply();
-        client.resource(Crds.kafkaUser()).serverSideApply();
-        client.resource(new CustomResourceDefinitionBuilder()
-                .withNewMetadata()
-                .withName("routes.route.openshift.io")
-            .endMetadata()
-            .withNewSpec()
-                .withScope("Namespaced")
-                .withGroup("route.openshift.io")
-                .addNewVersion()
-                    .withName("v1")
-                    .withNewSubresources()
-                        .withNewStatus()
-                        .endStatus()
-                    .endSubresources()
-                    .withNewSchema()
-                        .withNewOpenAPIV3Schema()
-                            .withType("object")
-                            .withXKubernetesPreserveUnknownFields(true)
-                        .endOpenAPIV3Schema()
-                    .endSchema()
-                    .withStorage(true)
-                    .withServed(true)
-                .endVersion()
-                .withNewNames()
-                    .withSingular("route")
-                    .withPlural("routes")
-                    .withKind("Route")
-                .endNames()
-            .endSpec()
-            .build())
-            .serverSideApply();
-
-        var allConsoles = client.resources(Console.class).inAnyNamespace();
-        var allKafkas = client.resources(Kafka.class).inAnyNamespace();
-        var allKafkaUsers = client.resources(KafkaUser.class).inAnyNamespace();
-        var allDeployments = client.resources(Deployment.class).inAnyNamespace().withLabels(ConsoleResource.MANAGEMENT_LABEL);
-        var allConfigMaps = client.resources(ConfigMap.class).inAnyNamespace().withLabels(ConsoleResource.MANAGEMENT_LABEL);
-        var allSecrets = client.resources(Secret.class).inAnyNamespace().withLabels(ConsoleResource.MANAGEMENT_LABEL);
-        var allIngresses = client.resources(Ingress.class).inAnyNamespace().withLabels(ConsoleResource.MANAGEMENT_LABEL);
-
-        allConsoles.delete();
-        allKafkas.delete();
-        allKafkaUsers.delete();
-        allDeployments.delete();
-        allConfigMaps.delete();
-        allSecrets.delete();
-        allIngresses.delete();
-
-        await().atMost(LIMIT).untilAsserted(() -> {
-            assertTrue(allConsoles.list().getItems().isEmpty());
-            assertTrue(allKafkas.list().getItems().isEmpty());
-            assertTrue(allKafkaUsers.list().getItems().isEmpty());
-            assertTrue(allDeployments.list().getItems().isEmpty());
-            assertTrue(allConfigMaps.list().getItems().isEmpty());
-            assertTrue(allSecrets.list().getItems().isEmpty());
-            assertTrue(allIngresses.list().getItems().isEmpty());
-        });
-
-        operator.start();
-
-        client.resource(new NamespaceBuilder()
-                .withNewMetadata()
-                    .withName("ns1")
-                    .withLabels(Map.of("streamshub-operator/test", "true"))
-                .endMetadata()
-                .build())
-            .serverSideApply();
-
-        kafkaCR = new KafkaBuilder()
-                .withNewMetadata()
-                    .withName("kafka-1")
-                    .withNamespace("ns1")
-                .endMetadata()
-                .withNewSpec()
-                    .withNewKafka()
-                        .addNewListener()
-                            .withName("listener1")
-                            .withType(KafkaListenerType.INGRESS)
-                            .withPort(9093)
-                            .withTls(true)
-                            .withAuth(new KafkaListenerAuthenticationScramSha512())
-                        .endListener()
-                    .endKafka()
-                .endSpec()
-                .withNewStatus()
-                    .withClusterId(UUID.randomUUID().toString())
-                    .addNewListener()
-                        .withName("listener1")
-                        .addNewAddress()
-                            .withHost("kafka-bootstrap.example.com")
-                            .withPort(9093)
-                        .endAddress()
-                    .endListener()
-                .endStatus()
-                .build();
-
-        kafkaCR = apply(client, kafkaCR);
-
-        client.resource(new NamespaceBuilder()
-                .withNewMetadata()
-                    .withName("ns2")
-                    .withLabels(Map.of("streamshub-operator/test", "true"))
-                .endMetadata()
-                .build())
-            .serverSideApply();
-    }
+class ConsoleReconcilerTest extends ConsoleReconcilerTestBase {
 
     @Test
     void testBasicConsoleReconciliation() {
-        Console consoleCR = new ConsoleBuilder()
-                .withMetadata(new ObjectMetaBuilder()
-                        .withName("console-1")
-                        .withNamespace("ns2")
-                        .build())
+        Console consoleCR = createConsole(new ConsoleBuilder()
                 .withNewSpec()
                     .withHostname("example.com")
                     .addNewKafkaCluster()
@@ -214,71 +59,23 @@ class ConsoleReconcilerTest {
                         .withNamespace(kafkaCR.getMetadata().getNamespace())
                         .withListener(kafkaCR.getSpec().getKafka().getListeners().get(0).getName())
                     .endKafkaCluster()
-                .endSpec()
-                .build();
+                .endSpec());
 
-        client.resource(consoleCR).create();
-
-        await().ignoreException(NullPointerException.class).atMost(LIMIT).untilAsserted(() -> {
-            var console = client.resources(Console.class)
-                    .inNamespace(consoleCR.getMetadata().getNamespace())
-                    .withName(consoleCR.getMetadata().getName())
-                    .get();
-            assertEquals(1, console.getStatus().getConditions().size());
-            var condition = console.getStatus().getConditions().iterator().next();
-            assertEquals(Condition.Types.READY, condition.getType(), () -> condition.toString());
-            assertEquals("False", condition.getStatus());
-            assertEquals("DependentsNotReady", condition.getReason());
-            assertTrue(condition.getMessage().contains("ConsoleIngress"));
-            assertTrue(condition.getMessage().contains("PrometheusDeployment"));
-        });
-
-        client.apps().deployments()
-            .inNamespace(consoleCR.getMetadata().getNamespace())
-            .withName("console-1-prometheus-deployment")
-            .editStatus(this::setReady);
-        LOGGER.info("Set ready replicas for Prometheus deployment");
-
+        awaitDependentsNotReady(consoleCR, "ConsoleIngress", "PrometheusDeployment");
         setConsoleIngressReady(consoleCR);
+        setDeploymentReady(consoleCR, PrometheusDeployment.NAME);
+        awaitDependentsNotReady(consoleCR, "ConsoleDeployment");
 
-        await().ignoreException(NullPointerException.class).atMost(LIMIT).untilAsserted(() -> {
-            var console = client.resources(Console.class)
-                    .inNamespace(consoleCR.getMetadata().getNamespace())
-                    .withName(consoleCR.getMetadata().getName())
-                    .get();
-            assertEquals(1, console.getStatus().getConditions().size(), () -> console.getStatus().getConditions().toString());
-            var condition = console.getStatus().getConditions().iterator().next();
-            assertEquals(Condition.Types.READY, condition.getType());
-            assertEquals("False", condition.getStatus());
-            assertEquals("DependentsNotReady", condition.getReason());
-            assertTrue(condition.getMessage().contains("ConsoleDeployment"));
-        });
-
-        var consoleDeployment = client.apps().deployments()
-                .inNamespace(consoleCR.getMetadata().getNamespace())
-                .withName("console-1-console-deployment")
-                .editStatus(this::setReady);
-        LOGGER.info("Set ready replicas for Console deployment");
-
+        var consoleDeployment = setDeploymentReady(consoleCR, ConsoleDeployment.NAME);
         // Images were not set in CR, so assert that the defaults were used
         var consoleContainers = consoleDeployment.getSpec().getTemplate().getSpec().getContainers();
+
         assertEquals(config.getValue("console.deployment.default-api-image", String.class),
                 consoleContainers.get(0).getImage());
         assertEquals(config.getValue("console.deployment.default-ui-image", String.class),
                 consoleContainers.get(1).getImage());
 
-        await().ignoreException(NullPointerException.class).atMost(LIMIT).untilAsserted(() -> {
-            var console = client.resources(Console.class)
-                    .inNamespace(consoleCR.getMetadata().getNamespace())
-                    .withName(consoleCR.getMetadata().getName())
-                    .get();
-            assertEquals(1, console.getStatus().getConditions().size());
-            var condition = console.getStatus().getConditions().iterator().next();
-            assertEquals(Condition.Types.READY, condition.getType());
-            assertEquals("True", condition.getStatus());
-            assertNull(condition.getReason());
-            assertEquals("All resources ready", condition.getMessage());
-        });
+        awaitReady(consoleCR);
     }
 
     @Test
@@ -998,47 +795,4 @@ class ConsoleReconcilerTest {
         assertEquals(registryMountPath, registryTrustPath.getValue());
     }
 
-    // Utility
-
-    private void assertConsoleConfig(Consumer<ConsoleConfig> assertion) {
-        await().ignoreException(NullPointerException.class).atMost(LIMIT).untilAsserted(() -> {
-            var consoleSecret = client.secrets().inNamespace("ns2").withName("console-1-" + ConsoleSecret.NAME).get();
-            assertNotNull(consoleSecret);
-            String configEncoded = consoleSecret.getData().get("console-config.yaml");
-            byte[] configDecoded = Base64.getDecoder().decode(configEncoded);
-            Logger.getLogger(getClass()).infof("config YAML: %s", new String(configDecoded));
-            ConsoleConfig consoleConfig = YAML.readValue(configDecoded, ConsoleConfig.class);
-            assertion.accept(consoleConfig);
-        });
-    }
-
-    private void setConsoleIngressReady(Console consoleCR) {
-        var consoleIngress = client.network().v1().ingresses()
-                .inNamespace(consoleCR.getMetadata().getNamespace())
-                .withName("console-1-console-ingress")
-                .get();
-
-        consoleIngress = consoleIngress.edit()
-                    .editOrNewStatus()
-                        .withNewLoadBalancer()
-                            .addNewIngress()
-                                .withHostname("ingress.example.com")
-                            .endIngress()
-                        .endLoadBalancer()
-                    .endStatus()
-                    .build();
-        client.resource(consoleIngress).patchStatus();
-        LOGGER.info("Set ingress status for Console ingress");
-    }
-
-    private Deployment setReady(Deployment deployment) {
-        int desiredReplicas = Optional.ofNullable(deployment.getSpec().getReplicas()).orElse(1);
-
-        return deployment.edit()
-            .editOrNewStatus()
-                .withReplicas(desiredReplicas)
-                .withReadyReplicas(desiredReplicas)
-            .endStatus()
-            .build();
-    }
 }
