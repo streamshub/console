4831808cd2c4326b9948a0958fdb7fae43013bb3
Michael Edgar <medgar@redhat.com>
2025-01-03 16:20:37 -0500

    Use TLS + truststore with Keycloak for API OIDC tests
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- api/src/test/java/com/github/streamshub/console/kafka/systemtest/deployment/KeycloakResourceManager.java
+++ api/src/test/java/com/github/streamshub/console/kafka/systemtest/deployment/KeycloakResourceManager.java
@@ -1,8 +1,10 @@
 package com.github.streamshub.console.kafka.systemtest.deployment;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.util.Map;
 
@@ -11,6 +13,8 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.Transferable;
+
+import com.github.streamshub.console.test.TlsHelper;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
@@ -29,26 +33,56 @@ public class KeycloakResourceManager implements QuarkusTestResourceLifecycleMana
             throw new UncheckedIOException(ioe);
         }
 
+        int port = 8443;
+        TlsHelper tls = TlsHelper.newInstance();
+        String keystorePath = "/opt/keycloak/keystore.p12";
+
         keycloak = new GenericContainer<>("quay.io/keycloak/keycloak:26.0")
                 .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("systemtests.keycloak"), true))
-                .withExposedPorts(8080)
+                .withExposedPorts(port)
                 .withEnv(Map.of(
                         "KC_BOOTSTRAP_ADMIN_USERNAME", "admin",
                         "KC_BOOTSTRAP_ADMIN_PASSWORD", "admin",
                         "PROXY_ADDRESS_FORWARDING", "true"))
                 .withCopyToContainer(
+                        Transferable.of(tls.getKeyStoreBytes()),
+                        keystorePath)
+                .withCopyToContainer(
                         Transferable.of(realmConfig),
                         "/opt/keycloak/data/import/console-realm.json")
-                .withCommand("start", "--hostname=localhost", "--http-enabled=true", "--import-realm")
-                .waitingFor(Wait.forHttp("/realms/console-authz").withStartupTimeout(Duration.ofMinutes(1)));
+                .withCommand(
+                        "start",
+                        "--hostname=localhost",
+                        "--http-enabled=false",
+                        "--https-key-store-file=%s".formatted(keystorePath),
+                        "--https-key-store-password=%s".formatted(String.copyValueOf(tls.getPassphrase())),
+                        "--import-realm"
+                )
+                .waitingFor(Wait.forHttps("/realms/console-authz")
+                        .allowInsecure()
+                        .withStartupTimeout(Duration.ofMinutes(1)));
+
+        File truststoreFile;
+
+        try {
+            truststoreFile = File.createTempFile("oidc-provider-trust", "." + tls.getTrustStore().getType());
+            //truststoreFile.deleteOnExit();
+            Files.write(truststoreFile.toPath(), tls.getTrustStoreBytes());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
 
         keycloak.start();
 
-        String urlTemplate = "http://localhost:%d/realms/console-authz";
-        var oidcUrl = urlTemplate.formatted(keycloak.getMappedPort(8080));
+        String urlTemplate = "https://localhost:%d/realms/console-authz";
+        var oidcUrl = urlTemplate.formatted(keycloak.getMappedPort(port));
         return Map.of(
                 "console.test.oidc-url", oidcUrl,
-                "console.test.oidc-issuer", urlTemplate.formatted(8080));
+                "console.test.oidc-host", "localhost:%d".formatted(port),
+                "console.test.oidc-issuer", urlTemplate.formatted(port),
+                "quarkus.tls.\"oidc-provider-trust\".trust-store.jks.path", truststoreFile.getAbsolutePath(),
+                "quarkus.tls.\"oidc-provider-trust\".trust-store.jks.password", String.copyValueOf(tls.getPassphrase())
+        );
     }
 
     @Override
