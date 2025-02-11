c26cde20df3f0ca8051ab726c213e86fe06def88
Michael Edgar <medgar@redhat.com>
2025-02-11 17:01:59 +0100

    tech preview disabled by default
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/utils/env.ts
+++ ui/utils/env.ts
@@ -2,4 +2,4 @@ export const isReadonly = process.env.NEXT_PUBLIC_CONSOLE_MODE === "read-only";
 
 export const isProductizedBuild = process.env.NEXT_PUBLIC_PRODUCTIZED_BUILD === "true";
 
-export const isTechPreview = (process.env.NEXT_PUBLIC_TECH_PREVIEW ?? "true") == "true";
+export const isTechPreview = (process.env.NEXT_PUBLIC_TECH_PREVIEW ?? "false") === "true";
