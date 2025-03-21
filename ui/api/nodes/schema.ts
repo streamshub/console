2796c04bdb1efc1084f89138326c6f7d6a55d8aa
hemahg <hhg@redhat.com>
2025-03-21 11:34:15 +0530

    UI changes for kraft
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/api/nodes/schema.ts
+++ ui/api/nodes/schema.ts
@@ -1,5 +1,30 @@
 import { z } from "zod";
 
+const NodeRoleSchema = z.union([z.literal("broker"), z.literal("controller")]);
+
+const NodePoolsSchema = z.union([
+  z.literal("brokers1"),
+  z.literal("controllers1"),
+  z.literal("dual1"),
+]);
+
+const BrokerStatusSchema = z.union([
+  z.literal("NotRunning"),
+  z.literal("Starting"),
+  z.literal("Recovery"),
+  z.literal("Running"),
+  z.literal("PendingControlledShutdown"),
+  z.literal("ShuttingDown"),
+  z.literal("Unknown"),
+]);
+
+const ControllerStatusSchema = z.union([
+  z.literal("QuorumLeader"),
+  z.literal("QuorumFollower"),
+  z.literal("QuorumFollowerLagged"),
+  z.literal("Unknown"),
+]);
+
 export const NodeSchema = z.object({
   id: z.string(),
   type: z.literal("nodes"),
@@ -7,38 +32,31 @@ export const NodeSchema = z.object({
     host: z.string().optional().nullable(),
     port: z.number().optional().nullable(),
     rack: z.string().optional().nullable(),
-    nodePool: z.string().optional().nullable(),
+    nodePool: NodePoolsSchema,
     kafkaVersion: z.string().optional().nullable(),
-    roles: z.array(z.enum([ "controller", "broker" ])).optional(),
-    metadataState: z.object({
-      status: z.enum([ "leader", "follower", "observer" ]),
-      logEndOffset: z.number(),
-      lastFetchTimestamp: z.string().nullable(),
-      lastCaughtUpTimestamp: z.string().nullable(),
-      lag: z.number(),
-      timeLag: z.number(),
-    }).optional().nullable(),
-    broker: z.object({
-      status: z.enum([
-        "NotRunning",
-        "Starting",
-        "Recovery",
-        "Running",
-        "PendingControlledShutdown",
-        "ShuttingDown",
-        "Unknown"
-      ]),
-      replicaCount: z.number(),
-      leaderCount: z.number(),
-    }).optional().nullable(),
-    controller: z.object({
-      status: z.enum([
-        "QuorumLeader",
-        "QuorumFollower",
-        "QuorumFollowerLagged",
-        "Unknown"
-      ]),
-    }).optional().nullable(),
+    roles: z.array(NodeRoleSchema),
+    metadataState: z
+      .object({
+        status: z.enum(["leader", "follower", "observer"]),
+        logEndOffset: z.number(),
+        lag: z.number(),
+      })
+      .optional()
+      .nullable(),
+    broker: z
+      .object({
+        status: BrokerStatusSchema,
+        replicaCount: z.number(),
+        leaderCount: z.number(),
+      })
+      .optional()
+      .nullable(),
+    controller: z
+      .object({
+        status: ControllerStatusSchema,
+      })
+      .optional()
+      .nullable(),
     storageUsed: z.number().optional().nullable(),
     storageCapacity: z.number().optional().nullable(),
   }),
@@ -56,12 +74,27 @@ export type NodesListMetaSummary = z.infer<typeof NodesListMetaSummary>;
 
 export const NodesResponseSchema = z.object({
   meta: z.object({
+<<<<<<< HEAD
     summary: NodesListMetaSummary,
+=======
+    page: z.object({
+      total: z.number(),
+      pageNumber: z.number().optional(),
+    }),
+  }),
+  links: z.object({
+    first: z.string().nullable(),
+    prev: z.string().nullable(),
+    next: z.string().nullable(),
+    last: z.string().nullable(),
+>>>>>>> 1f7997e (feat: UI changes for KRaft)
   }),
   data: z.array(NodeSchema),
 });
 
 export type NodeList = z.infer<typeof NodesResponseSchema>;
+
+export type NodeListResponse = z.infer<typeof NodeSchema>;
 
 const ConfigSchema = z.object({
   id: z.string().optional(),
@@ -80,6 +113,14 @@ const ConfigSchema = z.object({
 });
 
 export type NodeConfig = z.infer<typeof ConfigSchema>;
+
+export type BrokerStatus = z.infer<typeof BrokerStatusSchema>;
+
+export type NodePools = z.infer<typeof NodePoolsSchema>;
+
+export type ControllerStatus = z.infer<typeof ControllerStatusSchema>;
+
+export type NodeRoles = z.infer<typeof NodeRoleSchema>;
 
 export const ConfigResponseSchema = z.object({
   data: ConfigSchema,
--- ui/app/[locale]/(authorized)/kafka/[kafkaId]/nodes/NodesTable.tsx
+++ ui/app/[locale]/(authorized)/kafka/[kafkaId]/nodes/NodesTable.tsx
@@ -1,63 +1,126 @@
-"use client";
-
-import { Number } from "@/components/Format/Number";
-import { ResponsiveTable } from "@/components/Table";
 import {
-  ChartDonutUtilization,
-} from "@/libs/patternfly/react-charts";
+  BrokerStatus,
+  NodePools,
+  NodeRoles,
+  NodeList,
+  KafkaNode,
+} from "@/api/nodes/schema";
 import {
   ClipboardCopy,
   Flex,
   FlexItem,
   Label,
-  Text,
   TextContent,
   Tooltip,
+  Text,
+  SelectOption,
+  Icon,
 } from "@/libs/patternfly/react-core";
-import { HelpIcon } from "@/libs/patternfly/react-icons";
-import { useFormatBytes } from "@/utils/useFormatBytes";
-import { Icon } from "@patternfly/react-core";
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
-} from "@patternfly/react-icons";
-import { useFormatter } from "next-intl";
+  HelpIcon,
+} from "@/libs/patternfly/react-icons";
+import { useFormatter, useTranslations } from "next-intl";
 import Link from "next/link";
-import { useTranslations } from "next-intl";
+import { ReactNode } from "react";
+import { Number } from "@/components/Format/Number";
+import { ChartDonutUtilization } from "@/libs/patternfly/react-charts";
+import { useFormatBytes } from "@/utils/useFormatBytes";
+import { TableView, TableViewProps } from "@/components/Table/TableView";
 
-const columns = ["id", "roles", "status", "replicas", "rack", "nodePool"] as const;
+function splitAndCapitalize(value: string) {
+  // Split by uppercase letters, preserving them with a lookahead
+  const words = value.split(/(?=[A-Z])/);
 
-export type NodeStatus = {
-  stable: boolean,
-  description: string,
+  // Capitalize each word and join with a space
+  return words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+export const NodeListColumns = [
+  "id",
+  "roles",
+  "status",
+  "replicas",
+  "rack",
+  "nodePool",
+] as const;
+
+export type NodeListColumn = (typeof NodeListColumns)[number];
+
+const NodePoolLabel: Record<NodePools, ReactNode> = {
+  dual1: (
+    <SelectOption
+      value="both-pool"
+      description="Nodes role: broker, controller"
+    >
+      both-pool
+    </SelectOption>
+  ),
+  brokers1: (
+    <SelectOption value="B-pool-1" description="Nodes role: broker">
+      B-pool-1
+    </SelectOption>
+  ),
+  controllers1: (
+    <SelectOption value="C-pool-1" description="Nodes role: controller">
+      C-pool-1
+    </SelectOption>
+  ),
 };
 
-export type Node = {
-  id: string;
-  nodePool?: string;
-  roles: string[];
-  isLeader: boolean;
-  brokerStatus?: NodeStatus;
-  controllerStatus?: NodeStatus;
-  followers?: number;
-  leaders?: number;
-  rack?: string;
-  hostname?: string;
-  diskCapacity?: number;
-  diskUsage?: number;
-  kafkaVersion?: string;
+const NodeRoleLabel: Record<NodeRoles, ReactNode> = {
+  broker: <>Broker</>,
+  controller: <>Controller</>,
 };
 
-export function NodesTable({ nodes }: { nodes: Node[] }) {
+export type NodesTableProps = {
+  nodeList: KafkaNode[] | undefined;
+  page: number;
+  perPage: number;
+  filterNodePool: NodePools[] | undefined;
+  // filterStatus: BrokerStatus[] | undefined;
+  filterRole: NodeRoles[] | undefined;
+  onFilterNodePoolChange: (pool: NodePools[] | undefined) => void;
+  // onFilterStatusChange: (status: BrokerStatus[] | undefined) => void;
+  onFilterRoleChange: (role: NodeRoles[] | undefined) => void;
+} & Pick<
+  TableViewProps<NodeList, (typeof NodeListColumns)[number]>,
+  "isColumnSortable" | "onPageChange" | "onClearAllFilters"
+>;
 
+export function NodesTable({
+  isColumnSortable,
+  nodeList,
+  filterNodePool,
+  // filterStatus,
+  filterRole,
+  onFilterNodePoolChange,
+  onFilterRoleChange,
+  // onFilterStatusChange,
+  page,
+  perPage,
+  onPageChange,
+  onClearAllFilters,
+}: NodesTableProps) {
   const t = useTranslations();
   const format = useFormatter();
   const formatBytes = useFormatBytes();
+
   return (
-    <ResponsiveTable
+    <TableView
+      page={page}
+      perPage={perPage}
+      onPageChange={onPageChange}
+      isRowExpandable={() => true}
+      onClearAllFilters={onClearAllFilters}
+      data={nodeList}
+      emptyStateNoData={<></>}
+      emptyStateNoResults={<></>}
       ariaLabel={"Kafka nodes"}
-      columns={columns}
-      data={nodes}
+      columns={NodeListColumns}
       renderHeader={({ column, key, Th }) => {
         switch (column) {
           case "id":
@@ -70,11 +133,7 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
             return (
               <Th key={key}>
                 {t("nodes.replicas")}{" "}
-                <Tooltip
-                  content={
-                    t("nodes.replicas_tooltip")
-                  }
-                >
+                <Tooltip content={t("nodes.replicas_tooltip")}>
                   <HelpIcon />
                 </Tooltip>
               </Th>
@@ -83,11 +142,7 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
             return (
               <Th key={key}>
                 {t("nodes.rack")}{" "}
-                <Tooltip
-                  content={
-                    t("nodes.rack_tooltip")
-                  }
-                >
+                <Tooltip content={t("nodes.rack_tooltip")}>
                   <HelpIcon />
                 </Tooltip>
               </Th>
@@ -102,12 +157,12 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
             return (
               <Td key={key} dataLabel={"Node ID"}>
                 <Link href={`nodes/${row.id}`}>{row.id}</Link>
-                {row.isLeader && (
+                {row.attributes.metadataState?.status === "leader" && (
                   <Label
-                      isCompact={true}
-                      color={"green"}
-                      className={"pf-v5-u-ml-sm"}
-                    >
+                    isCompact={true}
+                    color={"green"}
+                    className={"pf-v5-u-ml-sm"}
+                  >
                     {t.rich("nodes.lead_controller")}
                   </Label>
                 )}
@@ -116,34 +171,54 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
           case "roles":
             return (
               <Td key={key} dataLabel={"Roles"}>
-                {
-                    row.roles.map((role, _) => {
-                        return <div key={role}>{role}</div>;
-                    })
-                }
+                {row.attributes.roles.map((role, _) => {
+                  return <div key={role}>{role}</div>;
+                })}
               </Td>
             );
           case "status":
             return (
               <Td key={key} dataLabel={"Status"}>
-                { row.controllerStatus &&
+                {row.attributes.controller && (
                   <div>
-                  <Icon status={row.controllerStatus.stable ? "success" : "warning"}>
-                    {row.controllerStatus.stable ? <CheckCircleIcon /> : <ExclamationCircleIcon />}
-                  </Icon>
-                  &nbsp;
-                  {row.controllerStatus?.description}
+                    <Icon
+                      status={
+                        row.attributes.controller.status !==
+                        "QuorumFollowerLagged"
+                          ? "success"
+                          : "warning"
+                      }
+                    >
+                      {row.attributes.controller.status !==
+                      "QuorumFollowerLagged" ? (
+                        <CheckCircleIcon />
+                      ) : (
+                        <ExclamationCircleIcon />
+                      )}
+                    </Icon>
+                    &nbsp;
+                    {splitAndCapitalize(row.attributes.controller.status)}
                   </div>
-                }
-                { row.brokerStatus &&
+                )}
+                {row.attributes.broker && (
                   <div>
-                  <Icon status={row.brokerStatus.stable ? "success" : "warning"}>
-                    {row.brokerStatus.stable ? <CheckCircleIcon /> : <ExclamationCircleIcon />}
-                  </Icon>
-                  &nbsp;
-                  {row.brokerStatus?.description}
+                    <Icon
+                      status={
+                        row.attributes.broker.status === "Running"
+                          ? "success"
+                          : "warning"
+                      }
+                    >
+                      {row.attributes.broker.status === "Running" ? (
+                        <CheckCircleIcon />
+                      ) : (
+                        <ExclamationCircleIcon />
+                      )}
+                    </Icon>
+                    &nbsp;
+                    {splitAndCapitalize(row.attributes.broker.status)}
                   </div>
-                }
+                )}
               </Td>
             );
           case "replicas":
@@ -151,8 +226,10 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
               <Td key={key} dataLabel={"Total replicas"}>
                 <Number
                   value={
-                    typeof row.followers == 'number' && typeof row.leaders == 'number'
-                      ? row.followers + row.leaders
+                    typeof row.attributes.broker?.leaderCount === "number" &&
+                    typeof row.attributes.broker?.replicaCount === "number"
+                      ? row.attributes.broker.leaderCount +
+                        row.attributes.broker.replicaCount
                       : undefined
                   }
                 />
@@ -161,98 +238,132 @@ export function NodesTable({ nodes }: { nodes: Node[] }) {
           case "rack":
             return (
               <Td key={key} dataLabel={"Rack"}>
-                {row.rack || "n/a"}
+                {row.attributes.rack || "n/a"}
               </Td>
             );
           case "nodePool":
             return (
               <Td key={key} dataLabel={"Node Pool"}>
-                {row.nodePool ?? "n/a"}
+                {row.attributes.nodePool ?? "n/a"}
               </Td>
             );
         }
       }}
-      isRowExpandable={() => true}
       getExpandedRow={({ row }) => {
-        let usedCapacity = (row.diskUsage !== undefined && row.diskCapacity !== undefined)
-          ? (row.diskUsage / row.diskCapacity) : undefined;
+        const diskCapacity = row.attributes.storageCapacity ?? undefined;
+        const diskUsage = row.attributes.storageUsed ?? undefined;
 
+        let usedCapacity =
+          diskUsage !== undefined && diskCapacity !== undefined
+            ? diskUsage / diskCapacity
+            : undefined;
         return (
           <Flex gap={{ default: "gap4xl" }} className={"pf-v5-u-p-xl"}>
             <FlexItem flex={{ default: "flex_1" }} style={{ maxWidth: "50%" }}>
               <TextContent>
-                <Text>
-                  {t.rich("nodes.host_name")}
-                </Text>
+                <Text>{t.rich("nodes.host_name")}</Text>
                 <Text>
                   <ClipboardCopy
                     isReadOnly={true}
                     variant={"expansion"}
                     isExpanded={true}
                   >
-                    {row.hostname || "n/a"}
+                    {row.attributes.host || "n/a"}
                   </ClipboardCopy>
                 </Text>
               </TextContent>
             </FlexItem>
             <FlexItem>
               <TextContent>
-                <Text>
-                  {t.rich("nodes.disk_usage")}
-                </Text>
+                <Text>{t.rich("nodes.disk_usage")}</Text>
               </TextContent>
               <div>
                 {usedCapacity !== undefined && (
-                    <div style={{ height: '300px', width: '230px' }}>
+                  <div style={{ height: "300px", width: "230px" }}>
                     <ChartDonutUtilization
-                        data={{
-                          x: "Used capacity",
-                          y: usedCapacity * 100,
-                        }}
-                        labels={({ datum }) =>
-                          datum.x
-                            ? `${datum.x}: ${format.number(datum.y / 100, {
+                      data={{
+                        x: "Used capacity",
+                        y: usedCapacity * 100,
+                      }}
+                      labels={({ datum }) =>
+                        datum.x
+                          ? `${datum.x}: ${format.number(datum.y / 100, {
                               style: "percent",
                             })}`
-                            : null
-                        }
-                        legendData={[
-                          { name: `Used capacity: ${formatBytes(row.diskUsage!)}` },
-                          { name: `Available: ${formatBytes(row.diskCapacity! - row.diskUsage!)}` },
-                        ]}
-                        legendOrientation="vertical"
-                        legendPosition="bottom"
-                        padding={{
-                            bottom: 75, // Adjusted to accommodate legend
-                            left: 20,
-                            right: 20,
-                            top: 20
-                          }}
-                        title={`${format.number(usedCapacity, {
-                            style: "percent",
-                          },
-                        )}`}
-                        subTitle={`of ${formatBytes(row.diskCapacity!)}`}
-                        thresholds={[{ value: 60 }, { value: 90 }]}
-                        height={300}
-                        width={230}
-                      />
-                    </div>
-                  )}
+                          : null
+                      }
+                      legendData={[
+                        { name: `Used capacity: ${formatBytes(diskUsage!)}` },
+                        {
+                          name: `Available: ${formatBytes(diskCapacity! - diskUsage!)}`,
+                        },
+                      ]}
+                      legendOrientation="vertical"
+                      legendPosition="bottom"
+                      padding={{
+                        bottom: 75, // Adjusted to accommodate legend
+                        left: 20,
+                        right: 20,
+                        top: 20,
+                      }}
+                      title={`${format.number(usedCapacity, {
+                        style: "percent",
+                      })}`}
+                      subTitle={`of ${formatBytes(diskCapacity!)}`}
+                      thresholds={[{ value: 60 }, { value: 90 }]}
+                      height={300}
+                      width={230}
+                    />
+                  </div>
+                )}
               </div>
             </FlexItem>
             <FlexItem>
               <TextContent>
-                <Text>
-                  {t.rich("nodes.kafka_version")}
-                </Text>
+                <Text>{t.rich("nodes.kafka_version")}</Text>
               </TextContent>
-              <div>
-                {row.kafkaVersion ?? "Unknown"}
-              </div>
+              <div>{row.attributes.kafkaVersion ?? "Unknown"}</div>
             </FlexItem>
           </Flex>
         );
+      }}
+      filters={{
+        "Node pool": {
+          type: "checkbox",
+          chips: filterNodePool || [],
+          onToggle: (pool) => {
+            const newPool = filterNodePool?.includes(pool)
+              ? filterNodePool.filter((p) => p !== pool)
+              : [...filterNodePool!, pool];
+            onFilterNodePoolChange(newPool);
+          },
+          onRemoveChip: (pool) => {
+            const newPool = (filterNodePool || []).filter((p) => p !== pool);
+            onFilterNodePoolChange(newPool);
+          },
+          onRemoveGroup: () => {
+            onFilterNodePoolChange(undefined);
+          },
+          options: NodePoolLabel,
+        },
+        Role: {
+          type: "checkbox",
+          chips: filterRole || [],
+          onToggle: (role) => {
+            const newRole = filterRole?.includes(role)
+              ? filterRole.filter((r) => r !== role)
+              : [...filterRole!, role];
+            onFilterNodePoolChange(newRole);
+          },
+          onRemoveChip: (role) => {
+            const newRole = (filterRole || []).filter((r) => r !== role);
+            onFilterRoleChange(newRole);
+          },
+          onRemoveGroup: () => {
+            onFilterRoleChange(undefined);
+          },
+          options: NodeRoleLabel,
+        },
       }}
     />
   );
