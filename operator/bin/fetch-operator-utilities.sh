e34ee40066ba02c3505cfa637f0a54e5ff5c665b
Michael Edgar <medgar@redhat.com>
2025-03-25 15:03:18 -0400

    fix: correct operator framework utility fetch URLs
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- operator/bin/fetch-operator-utilities.sh
+++ operator/bin/fetch-operator-utilities.sh
@@ -4,15 +4,15 @@ set -Eeuo pipefail
 
 SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 
-OPERATOR_SDK_VERSION=${OPERATOR_SDK_VERSION:-v1.39.2}
-OPM_VERSION=${OPM_VERSION:-v1.51.1}
+OPERATOR_SDK_VERSION="${OPERATOR_SDK_VERSION:-v1.39.2}"
+OPM_VERSION="${OPM_VERSION:-v1.51.0}"
 
-curl -L -o operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk_linux-amd64
+curl -L -s -o operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk_linux_amd64
 chmod +x operator-sdk
 sudo cp -v operator-sdk /usr/bin/
 rm -vf operator-sdk
 
-curl -L -o opm https://github.com/operator-framework/operator-registry/releases/download/${OPM_VERSION}/linux-amd64-opm
+curl -L -s -o opm https://github.com/operator-framework/operator-registry/releases/download/${OPM_VERSION}/linux-amd64-opm
 chmod +x opm
 sudo cp -v opm /usr/bin/
 rm -vf opm
