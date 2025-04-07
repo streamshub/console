99f19a944ab6bb92d1b808c32fef7fcbbbb09944
Jan Kalinic <jkalinic@redhat.com>
2025-04-07 17:56:19 +0200

    [ST] Console installation (#1598)
    Console yaml and olm based installation
    
    ---------
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>

---
+++ systemtests/src/main/java/com/github/streamshub/systemtests/utils/WaitUtils.java
@@ -0,0 +1,28 @@
+package com.github.streamshub.systemtests.utils;
+
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.readiness.Readiness;
+import io.skodjob.testframe.TestFrameConstants;
+import io.skodjob.testframe.wait.Wait;
+
+import static com.github.streamshub.systemtests.utils.ResourceUtils.listKubeResourcesByPrefix;
+
+public class WaitUtils {
+
+    private WaitUtils() {}
+
+    // ------------
+    // Deployment
+    // ------------
+    public static void waitForDeploymentWithPrefixIsReady(String namespaceName, String deploymentNamePrefix) {
+        Wait.until(String.format("creation of Deployment with prefix: %s in Namespace: %s", deploymentNamePrefix, namespaceName),
+            TestFrameConstants.GLOBAL_POLL_INTERVAL_1_SEC, TestFrameConstants.GLOBAL_TIMEOUT_MEDIUM,
+            () -> {
+                Deployment dep = listKubeResourcesByPrefix(Deployment.class, namespaceName, deploymentNamePrefix).get(0);
+                if (dep == null) {
+                    return false;
+                }
+                return Readiness.isDeploymentReady(dep);
+            });
+    }
+}
