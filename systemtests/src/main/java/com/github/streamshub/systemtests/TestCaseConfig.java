d1bd35d05fab07d26239f5b5a5b308e591e167ac
jkalinic <jkalinic@redhat.com>
2025-04-23 13:48:03 +0200

    improvements
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtests/src/main/java/com/github/streamshub/systemtests/TestCaseConfig.java
+++ systemtests/src/main/java/com/github/streamshub/systemtests/TestCaseConfig.java
@@ -8,8 +8,8 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+import java.lang.reflect.Method;
 import java.util.Locale;
-
 
 public class TestCaseConfig {
     private final String testName;
@@ -20,8 +20,15 @@ public class TestCaseConfig {
     private final Page page;
 
     public TestCaseConfig(ExtensionContext extensionContext) {
-        this.testName = extensionContext.getTestMethod().isPresent() ? extensionContext.getTestMethod().get().getName() : extensionContext.getTestClass().get().getSimpleName();
-        this.namespace = extensionContext.getTestClass().isPresent() ?  extensionContext.getTestClass().get().getSimpleName().toLowerCase(Locale.ENGLISH) + "-" + Utils.hashStub(testName) : "nullClass";
+        this.testName = extensionContext.getTestMethod()
+            .map(Method::getName)
+            .or(() -> extensionContext.getTestClass().map(Class::getSimpleName))
+            .orElseThrow();
+        this.namespace = extensionContext.getTestClass()
+            .map(Class::getSimpleName)
+            .map(name -> name.toLowerCase(Locale.ENGLISH) + "-" + Utils.hashStub(testName))
+            .orElse("nullClass");
+
         this.playwright = Playwright.create();
         this.browser = PwUtils.createBrowser(playwright);
         this.context = browser.newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true));
