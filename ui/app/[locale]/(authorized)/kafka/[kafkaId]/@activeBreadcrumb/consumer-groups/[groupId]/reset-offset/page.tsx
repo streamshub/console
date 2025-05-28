10421ee9497eb6eaeebb0e7f01355f7f212bdc99
hemahg <hhg@redhat.com>
2025-05-28 19:36:48 +0530

    resolve merge conflict
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/app/[locale]/(authorized)/kafka/[kafkaId]/@activeBreadcrumb/consumer-groups/[groupId]/reset-offset/page.tsx
+++ ui/app/[locale]/(authorized)/kafka/[kafkaId]/@activeBreadcrumb/consumer-groups/[groupId]/reset-offset/page.tsx
@@ -1,7 +1,12 @@
 import { KafkaConsumerGroupMembersParams } from "@/app/[locale]/(authorized)/kafka/[kafkaId]/consumer-groups/[groupId]/KafkaConsumerGroupMembers.params";
 import { BreadcrumbLink } from "@/components/Navigation/BreadcrumbLink";
 import RichText from "@/components/RichText";
-import { BreadcrumbItem } from "@/libs/patternfly/react-core";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Tooltip,
+} from "@/libs/patternfly/react-core";
+import { HomeIcon } from "@/libs/patternfly/react-icons";
 import { useTranslations } from "next-intl";
 
 export default function ConsumerGroupsActiveBreadcrumb({
@@ -10,21 +15,30 @@ export default function ConsumerGroupsActiveBreadcrumb({
   params: KafkaConsumerGroupMembersParams;
 }) {
   const t = useTranslations();
-
-  return [
-    <BreadcrumbLink
-      key={"cg"}
-      href={`/kafka/${kafkaId}/consumer-groups`}
-      showDivider={true}
-    >
-      Consumer groups
-    </BreadcrumbLink>,
-    <BreadcrumbItem key={"cgm"} showDivider={true} isActive={true}>
-      {decodeURIComponent(groupId) === "+" ? (
-        <RichText>{(tags) => t.rich("common.empty_name", tags)}</RichText>
-      ) : (
-        groupId
-      )}
-    </BreadcrumbItem>,
-  ];
+  return (
+    <Breadcrumb>
+      <BreadcrumbItem key="home" to="/" showDivider>
+        <Tooltip content={t("breadcrumbs.view_all_kafka_clusters")}>
+          <HomeIcon />
+        </Tooltip>
+      </BreadcrumbItem>
+      <BreadcrumbItem
+        key="overview"
+        to={`/kafka/${kafkaId}/overview`}
+        showDivider
+      >
+        {t("breadcrumbs.overview")}
+      </BreadcrumbItem>
+      <BreadcrumbLink
+        key={"cg"}
+        href={`/kafka/${kafkaId}/consumer-groups`}
+        showDivider={true}
+      >
+        {t("breadcrumbs.consumer_groups")}
+      </BreadcrumbLink>
+      <BreadcrumbItem key={"cgm"} showDivider={true} isActive={true}>
+        {decodeURIComponent(groupId) === "+" ? <RichText>{(tags) => t.rich("common.empty_name", tags)}</RichText> : groupId}
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
 }
