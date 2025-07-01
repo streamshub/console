8f9cb0e1c1e4253c55a63a12cfd8d052be4a5085
hemahg <hhg@redhat.com>
2025-07-01 11:31:34 +0530

    fix formating
    Signed-off-by: hemahg <hhg@redhat.com>

--- api/src/main/java/com/github/streamshub/console/api/service/KafkaClusterService.java
+++ api/src/main/java/com/github/streamshub/console/api/service/KafkaClusterService.java
@@ -314,27 +314,27 @@ public class KafkaClusterService {
     }
 
 	void setKafkaClusterStatus(KafkaCluster cluster, Kafka kafka) {
-	    Optional.ofNullable(kafka.getStatus()).ifPresentOrElse(status -> {
-	        String kafkaVersion = Optional.ofNullable(status.getKafkaVersion()).orElseGet(() -> {
-	            if (kafka.getSpec() != null && kafka.getSpec().getKafka() != null) {
-	                return kafka.getSpec().getKafka().getVersion();
-	            }
-	            return null;
-	        });
-	        cluster.kafkaVersion(kafkaVersion);
-	        Optional.ofNullable(status.getConditions()).ifPresent(conditions -> {
-	            cluster.conditions(conditions.stream().map(Condition::new).toList());
+		Optional.ofNullable(kafka.getStatus()).ifPresentOrElse(status -> {
+			String kafkaVersion = Optional.ofNullable(status.getKafkaVersion()).orElseGet(() -> {
+				if (kafka.getSpec() != null && kafka.getSpec().getKafka() != null) {
+					return kafka.getSpec().getKafka().getVersion();
+				}
+				return null;
+			});
+			cluster.kafkaVersion(kafkaVersion);
+			Optional.ofNullable(status.getConditions()).ifPresent(conditions -> {
+				cluster.conditions(conditions.stream().map(Condition::new).toList());
 
-	            conditions.stream().filter(c -> "NotReady".equals(c.getType()) && "True".equals(c.getStatus()))
-	            .findFirst().ifPresentOrElse(c -> cluster.status("NotReady"), () -> cluster.status("Ready"));
-	        });
-	        Optional.ofNullable(status.getKafkaNodePools())
-	        .ifPresent(pools -> cluster.nodePools(pools.stream().map(pool -> pool.getName()).toList()));
-	    }, () -> {
-	        if (kafka.getSpec() != null && kafka.getSpec().getKafka() != null) {
-	            cluster.kafkaVersion(kafka.getSpec().getKafka().getVersion());
-	        }
-	    });
+				conditions.stream().filter(c -> "NotReady".equals(c.getType()) && "True".equals(c.getStatus()))
+						.findFirst().ifPresentOrElse(c -> cluster.status("NotReady"), () -> cluster.status("Ready"));
+			});
+			Optional.ofNullable(status.getKafkaNodePools())
+					.ifPresent(pools -> cluster.nodePools(pools.stream().map(pool -> pool.getName()).toList()));
+		}, () -> {
+			if (kafka.getSpec() != null && kafka.getSpec().getKafka() != null) {
+				cluster.kafkaVersion(kafka.getSpec().getKafka().getVersion());
+			}
+		});
 	}
 
     KafkaCluster setManaged(KafkaCluster cluster) {
