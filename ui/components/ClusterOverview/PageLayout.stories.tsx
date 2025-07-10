ba05684fdb77abf5e10e168a11eb9f60e199595b
hemahg <hhg@redhat.com>
2025-07-11 00:52:00 +0530

    change only compositestories to import from react
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/components/ClusterOverview/PageLayout.stories.tsx
+++ ui/components/ClusterOverview/PageLayout.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 import { composeStories } from "@storybook/react";
 import * as ClusterCardStories from "./ClusterCard.stories";
 import { PageLayout } from "./PageLayout";
