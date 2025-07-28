ced3c40db70833a605b76450b8a676a132a29dda
jkalinic <jkalinic@redhat.com>
2025-07-28 22:01:56 +0200

    add test for earliest offset reset
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtests/src/test/java/com/github/streamshub/systemtests/ConsumerST.java
+++ systemtests/src/test/java/com/github/streamshub/systemtests/ConsumerST.java
@@ -11,14 +11,19 @@ import com.github.streamshub.systemtests.utils.WaitUtils;
 import com.github.streamshub.systemtests.utils.playwright.PwPageUrls;
 import com.github.streamshub.systemtests.utils.playwright.PwUtils;
 import com.github.streamshub.systemtests.utils.resourceutils.KafkaClientsUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.KafkaConsumerUtils;
 import com.github.streamshub.systemtests.utils.resourceutils.KafkaNamingUtils;
 import com.github.streamshub.systemtests.utils.resourceutils.KafkaTopicUtils;
 import com.github.streamshub.systemtests.utils.resourceutils.KafkaUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.ResourceUtils;
+import io.fabric8.kubernetes.api.model.Pod;
 import io.skodjob.testframe.resources.KubeResourceManager;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConsumerST extends AbstractST {
     private static final Logger LOGGER = LogWrapper.getLogger(ConsumerST.class);
@@ -57,8 +62,21 @@ public class ConsumerST extends AbstractST {
         PwUtils.waitForContainsText(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_CONSUMER_GROUP_NAME, consumerGroupName, true);
         PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_DROPDOWN_BUTTON);
         PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_EARLIEST_OFFSET);
-
+        // Dryrun
         PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_DRY_RUN_BUTTON);
+        // Check selector for text
+        String dryRunCommand = String.format("$ kafka-consumer-groups --bootstrap-server ${bootstrap-Server} --group %s --reset-offsets --all-topics --to-earliest --dry-run", consumerGroupName);
+        PwUtils.waitForContainsAttribute(tcc, ConsumerGroupsPageSelectors.CGPS_DRY_RUN_COMMAND, dryRunCommand, Constants.VALUE_ATTRIBUTE, true);
+        PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_BACK_TO_EDIT_OFFSET_BUTTON);
+
+        // Reset for real now
+        PwUtils.waitForContainsText(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_CONSUMER_GROUP_NAME, consumerGroupName, true);
+        PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_DROPDOWN_BUTTON);
+        PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_EARLIEST_OFFSET);
+        PwUtils.waitForLocatorAndClick(tcc, ConsumerGroupsPageSelectors.CGPS_RESET_PAGE_OFFSET_RESET_BUTTON);
+        // Check offset
+        String brokerPodName = ResourceUtils.listKubeResourcesByPrefix(Pod.class, tcc.namespace(), KafkaNamingUtils.brokerPodNamePrefix(tcc.kafkaName())).get(0).getMetadata().getName();
+        assertEquals(KafkaConsumerUtils.getConsumerGroupOffset(tcc.namespace(), tcc.kafkaName(), brokerPodName, consumerGroupName, clients), "0");
     }
 
     @BeforeEach
