452df890c09f475f9c17d6af4c7673461a5350c3
Michael Edgar <medgar@redhat.com>
2025-08-28 15:51:17 -0400

    fix: ensure DateTime tests work regardless of the OS time zone
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/components/Format/DateTime.stories.tsx
+++ ui/components/Format/DateTime.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/nextjs";
 import { expect, waitFor, within } from "storybook/test";
 import { DateTime } from "./DateTime";
 
+const ORIGINAL_ENV = process.env;
+
 const meta: Meta<typeof DateTime> = {
   component: DateTime,
   args: {
@@ -32,27 +34,41 @@ export const Default: Story = {};
 
 export const DateTimeStringUTC: Story = {
   args: {
-    value: "2025-04-01T12:00:00-04:00", // A static date value
+    value: "2025-04-01T00:00:00-07:00", // A static date value
     utc: true,
+  },
+  beforeEach: () => {
+    // UTC-7 for daylight / UTC-8 for standard
+    process.env = { ...ORIGINAL_ENV, TZ: "America/Los_Angeles" }
+  },
+  afterEach: () => {
+    process.env = ORIGINAL_ENV;
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     await waitFor(() => {
       // Check that the date was adjusted to UTC
-      expect(canvas.getByText("2025-04-01 16:00:00Z")).toBeInTheDocument();
+      expect(canvas.getByText("2025-04-01 07:00:00Z")).toBeInTheDocument();
     });
   },
 };
 
 export const DateTimeStringLocal: Story = {
   args: {
-    value: "2025-04-01T16:00:00Z", // A static date value
+    value: "2025-04-01T14:00:00Z", // A static date value
+  },
+  beforeEach: () => {
+    // UTC-4 for daylight / UTC-5 for standard
+    process.env = { ...ORIGINAL_ENV, TZ: "America/New_York" }
+  },
+  afterEach: () => {
+    process.env = ORIGINAL_ENV;
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     await waitFor(() => {
       // Check that the date was adjusted to UTC
-      expect(canvas.getByText("2025-04-01 12:00:00-04:00")).toBeInTheDocument();
+      expect(canvas.getByText("2025-04-01 10:00:00-04:00")).toBeInTheDocument();
     });
   },
 };
