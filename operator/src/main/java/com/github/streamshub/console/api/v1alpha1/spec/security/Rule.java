bfe41fd951272f9dbcf2c49faf5b2a3b0d1e91cb
Michael Edgar <medgar@redhat.com>
2025-09-05 08:16:35 -0400

    Add rule model validation, example, & custom resource docs
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- operator/src/main/java/com/github/streamshub/console/api/v1alpha1/spec/security/Rule.java
+++ operator/src/main/java/com/github/streamshub/console/api/v1alpha1/spec/security/Rule.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import io.fabric8.generator.annotation.Required;
 import io.sundr.builder.annotations.Buildable;
@@ -11,21 +12,25 @@ import io.sundr.builder.annotations.Buildable;
 @Buildable(editableEnabled = false)
 public class Rule {
 
-    /**
-     * Resources to which this rule applies (required)
-     */
     @Required
+    @JsonPropertyDescription("Resources to which this rule applies (required)")
     List<String> resources;
 
-    /**
-     * Specific resource names to which this rule applies (optional)
-     */
+    @JsonPropertyDescription("""
+            Specific resource names to which this rule applies (optional).
+
+            The entries in the array may be one of the following:
+            1. the literal name of a resource that must match exactly for
+               the rule to apply
+            2. a name prefix ending with a wildcard (*) that must match
+               the part of a resource name up to the wildcard
+            3. a valid regular expression starting and ending with a slash (/).
+               The rule is applied to resources that match the pattern.
+            """)
     List<String> resourceNames;
 
-    /**
-     * Privileges/actions that may be performed for subjects having this rule
-     */
     @Required
+    @JsonPropertyDescription("Privileges/actions that may be performed for subjects linked to this rule")
     List<Privilege> privileges;
 
     public List<String> getResources() {
