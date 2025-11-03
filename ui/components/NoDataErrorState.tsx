73b8806ed8bcecec20efbde14cb966ca1f52c389
hemahg <hhg@redhat.com>
2025-11-03 20:21:21 +0530

    Add unique key for each error
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/components/NoDataErrorState.tsx
+++ ui/components/NoDataErrorState.tsx
@@ -8,6 +8,7 @@ import {
 import { ErrorCircleOIcon, BanIcon } from "@/libs/patternfly/react-icons";
 import { ApiError } from "@/api/api";
 import { useEffect, useRef } from "react";
+import React from "react";
 
 export function NoDataErrorState({ errors }: { errors: ApiError[] }) {
   const errorIcon = errors[0].status === "403" ? BanIcon : ErrorCircleOIcon;
@@ -31,13 +32,11 @@ export function NoDataErrorState({ errors }: { errors: ApiError[] }) {
     >
       <EmptyStateBody>
         <>
-          {errors.map((err) => {
-            return (
-              <>
-                {err.title}: {err.detail} {err.code && <>({err.code})</>}
-              </>
-            );
-          })}
+          {errors.map((err) => (
+            <React.Fragment key={err.id}>
+              {err.title}: {err.detail} {err.code && <>({err.code})</>}
+            </React.Fragment>
+          ))}
         </>
       </EmptyStateBody>
     </EmptyState>
