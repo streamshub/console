bc046cda2adc9232136021a476ced82d9957186d
hemahg <hhg@redhat.com>
2025-11-03 13:01:07 +0530

    fix window reference error
    Signed-off-by: hemahg <hhg@redhat.com>

--- ui/components/NoDataErrorState.tsx
+++ ui/components/NoDataErrorState.tsx
@@ -7,23 +7,18 @@ import {
 } from "@/libs/patternfly/react-core";
 import { ErrorCircleOIcon, BanIcon } from "@/libs/patternfly/react-icons";
 import { ApiError } from "@/api/api";
+import { useEffect, useRef } from "react";
 
 export function NoDataErrorState({ errors }: { errors: ApiError[] }) {
-  let errorIcon;
+  const errorIcon = errors[0].status === "403" ? BanIcon : ErrorCircleOIcon;
+  const hasSignedOut = useRef(false);
 
-  switch (errors[0].status ?? "400") {
-    case "401":
-      // Force sign out in attempt to re-establish an authenticated sessions
+  useEffect(() => {
+    if (errors[0].status === "401" && !hasSignedOut.current) {
+      hasSignedOut.current = true;
       signOut();
-      // Fall through
-    case "403":
-      errorIcon = BanIcon;
-      break;
-    default:
-      errorIcon = ErrorCircleOIcon;
-      break;
-  }
-
+    }
+  }, [errors]);
   return (
     <EmptyState
       titleText={
