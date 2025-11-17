59a5e69af8e7738af13833efe0d2b04a667d2870
Michael Edgar <medgar@redhat.com>
2025-11-17 08:45:27 -0500

    Add options in form to remove consumer group offsets
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/app/[locale]/(authorized)/kafka/[kafkaId]/consumer-groups/[groupId]/types.ts
+++ ui/app/[locale]/(authorized)/kafka/[kafkaId]/consumer-groups/[groupId]/types.ts
@@ -2,6 +2,6 @@ export type TopicSelection = "allTopics" | "selectedTopic";
 
 export type partitionSelection = "allPartitions" | "selectedPartition";
 
-export type OffsetValue = "custom" | "latest" | "earliest" | "specificDateTime";
+export type OffsetValue = "custom" | "latest" | "earliest" | "specificDateTime" | "delete" ;
 
 export type DateTimeFormatSelection = "ISO" | "Epoch";
