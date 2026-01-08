defae53dfbade88aaa39316ea5766d40859c25e7
hemahg <hhg@redhat.com>
2026-01-08 19:22:01 +0530

    Improve test coverage
    Signed-off-by: hemahg <hhg@redhat.com>

--- api/src/test/java/com/github/streamshub/console/api/KafkaClustersResourceIT.java
+++ api/src/test/java/com/github/streamshub/console/api/KafkaClustersResourceIT.java
@@ -208,14 +208,13 @@ class KafkaClustersResourceIT {
             .body("data.find { it.attributes.name == 'test-kafka1'}.attributes.listeners", hasItem(allOf(
                     hasEntry("bootstrapServers", k1Bootstrap),
                     hasEntry("authType", "custom"))))
+            .body("data.find { it.attributes.name == 'test-kafka1' }.meta.kind", is("kafkas.kafka.strimzi.io")) 
             .body("data.find { it.attributes.name == 'test-kafka2'}.attributes.status", is("NotReady"))
             .body("data.find { it.attributes.name == 'test-kafka2'}.attributes.listeners", hasItem(allOf(
                     hasEntry(equalTo("bootstrapServers"), equalTo(k2Bootstrap)),
                     hasEntry(equalTo("authType"), nullValue(String.class)))))
             .body("data.find { it.attributes.name == 'test-kafkaY'}.attributes.status", is(nullValue()))
-            .body("data.find { it.attributes.name == 'test-kafkaY'}.attributes.listeners", is(nullValue()))
-            .body("data.find { it.attributes.name == 'test-kafkaY' }.meta.kind", is(nullValue()));
-
+            .body("data.find { it.attributes.name == 'test-kafkaY'}.attributes.listeners", is(nullValue()));
     }
 
     @Test
