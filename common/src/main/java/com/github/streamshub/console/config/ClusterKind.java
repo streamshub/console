772dadefaa6c28379d2a35dfe8ce791afe3d8fe5
hemahg <hhg@redhat.com>
2026-01-08 23:08:30 +0530

    update indentation
    Signed-off-by: hemahg <hhg@redhat.com>

--- common/src/main/java/com/github/streamshub/console/config/ClusterKind.java
+++ common/src/main/java/com/github/streamshub/console/config/ClusterKind.java
@@ -4,27 +4,27 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ClusterKind {
-  STRIMI_KAFKA("kafkas.kafka.strimzi.io"),
-  KROXYLICIOUS_VIRTUAL_KAFKA_CLUSTER("virtualkafkaclusters.kroxylicious.io");
+    STRIMI_KAFKA("kafkas.kafka.strimzi.io"),
+    KROXYLICIOUS_VIRTUAL_KAFKA_CLUSTER("virtualkafkaclusters.kroxylicious.io");
 
-  private final String value;
+    private final String value;
 
-  private ClusterKind(String value) {
-    this.value = value;
-  }
-
-  @JsonValue
-  public String getValue() {
-    return value;
-  }
-
-  @JsonCreator
-  public static ClusterKind forValue(String value) {
-    for (var kind : values()) {
-      if (kind.value.equals(value)) {
-        return kind;
-      }
+    private ClusterKind(String value) {
+        this.value = value;
     }
-    throw new IllegalArgumentException("Unknown cluster kind: " + value);
-  }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static ClusterKind forValue(String value) {
+        for (var kind : values()) {
+            if (kind.value.equals(value)) {
+                return kind;
+            }
+        }
+        throw new IllegalArgumentException("Unknown cluster kind: " + value);
+    }
 }
