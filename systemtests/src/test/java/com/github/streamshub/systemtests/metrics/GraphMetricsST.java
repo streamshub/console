f498e2b688397c82392b77c1722b65b458f08761
jkalinic <jkalinic@redhat.com>
2026-02-05 15:20:32 +0100

    [wip] test graphs
    Signed-off-by: jkalinic <jkalinic@redhat.com>

---
+++ systemtests/src/test/java/com/github/streamshub/systemtests/metrics/GraphMetricsST.java
@@ -0,0 +1,104 @@
+package com.github.streamshub.systemtests.metrics;
+
+
+import com.github.streamshub.systemtests.AbstractST;
+import com.github.streamshub.systemtests.TestCaseConfig;
+import com.github.streamshub.systemtests.annotations.SetupTestBucket;
+import com.github.streamshub.systemtests.annotations.TestBucket;
+import com.github.streamshub.systemtests.clients.KafkaClients;
+import com.github.streamshub.systemtests.clients.KafkaClientsBuilder;
+import com.github.streamshub.systemtests.constants.Constants;
+import com.github.streamshub.systemtests.constants.TestTags;
+import com.github.streamshub.systemtests.logs.LogWrapper;
+import com.github.streamshub.systemtests.setup.console.ConsoleInstanceSetup;
+import com.github.streamshub.systemtests.setup.strimzi.KafkaSetup;
+import com.github.streamshub.systemtests.utils.Utils;
+import com.github.streamshub.systemtests.utils.WaitUtils;
+import com.github.streamshub.systemtests.utils.playwright.PwPageUrls;
+import com.github.streamshub.systemtests.utils.playwright.PwUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.KafkaClientsUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.KafkaNamingUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.KafkaTopicUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.KafkaUtils;
+import com.github.streamshub.systemtests.utils.resourceutils.NamespaceUtils;
+import io.skodjob.testframe.resources.KubeResourceManager;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@Tag(TestTags.REGRESSION)
+
+public class GraphMetricsST extends AbstractST {
+    private static final Logger LOGGER = LogWrapper.getLogger(GraphMetricsST.class);
+    private static final String KAFKA_CPU_METRICS_BUCKET = "KafkaCPUBucket";
+    private static final String KAFKA_METRICS_TOPIC_PREFIX = "kafka-metrics";
+    private static final int KAFKA_METRICS_TOPIC_COUNT = 1;
+
+    // Shared
+    protected TestCaseConfig tcc;
+    private static final int MESSAGE_COUNT = 100 * Constants.MESSAGE_COUNT_HIGH;
+
+
+    @TestBucket(KAFKA_CPU_METRICS_BUCKET)
+    @Test
+    void testKafkaCpu() {
+        LOGGER.info("Start");
+        tcc.page().navigate(PwPageUrls.getOverviewPage(tcc, tcc.kafkaName()));
+
+
+        LOGGER.info("end");
+    }
+
+    @SetupTestBucket(KAFKA_CPU_METRICS_BUCKET)
+    public void setupKafkaCpuMetrics() {
+        tcc.setMessageCount(MESSAGE_COUNT);
+
+        LOGGER.info("Prepare consumer offset scenario by creating topic(s) and then producing and consuming messages");
+
+        List<String> kafkaTopicNames = KafkaTopicUtils.setupTopicsAndReturn(tcc.namespace(), tcc.kafkaName(), KAFKA_METRICS_TOPIC_PREFIX, KAFKA_METRICS_TOPIC_COUNT, true, 1, 1, 1)
+            .stream()
+            .map(kt -> kt.getMetadata().getName())
+            .toList();
+
+        for (String kafkaTopicName : kafkaTopicNames) {
+            KafkaClients clients = new KafkaClientsBuilder()
+                .withNamespaceName(tcc.namespace())
+                .withTopicName(kafkaTopicName)
+                .withMessageCount(tcc.messageCount())
+                .withDelayMs(0)
+                .withProducerName(KafkaNamingUtils.producerName(kafkaTopicName))
+                .withConsumerName(KafkaNamingUtils.consumerName(kafkaTopicName))
+                .withConsumerGroup(KafkaNamingUtils.consumerGroupName(kafkaTopicName))
+                .withBootstrapAddress(KafkaUtils.getPlainScramShaBootstrapAddress(tcc.kafkaName()))
+                .withUsername(tcc.kafkaUserName())
+                .withAdditionalConfig(KafkaClientsUtils.getScramShaConfig(tcc.namespace(), tcc.kafkaUserName(), SecurityProtocol.SASL_PLAINTEXT))
+                .build();
+
+            KubeResourceManager.get().createResourceWithWait(clients.producer(), clients.consumer());
+            WaitUtils.waitForClientsSuccess(clients);
+        }
+
+        LOGGER.info("Reset consumer offset scenario ready");
+    }
+
+    @BeforeAll
+    void testClassSetup() {
+        // Init test case config based on the test context
+        tcc = Utils.getTestCaseConfig();
+        // Prepare test environment
+        NamespaceUtils.prepareNamespace(tcc.namespace());
+        KafkaSetup.setupDefaultKafkaIfNeeded(tcc.namespace(), tcc.kafkaName());
+        ConsoleInstanceSetup.setupIfNeeded(ConsoleInstanceSetup.getDefaultConsoleInstance(tcc.namespace(), tcc.consoleInstanceName(), tcc.kafkaName(), tcc.kafkaUserName()).build());
+        PwUtils.login(tcc);
+    }
+
+    @AfterAll
+    void testClassTeardown() {
+        tcc.playwright().close();
+    }
+}
