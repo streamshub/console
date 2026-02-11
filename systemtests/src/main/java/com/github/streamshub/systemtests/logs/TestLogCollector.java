66ba5d801dbf3bcf08582d40fce2190d40c2d2f5
Jan Kalinic <jkalinic@redhat.com>
2026-02-11 20:13:24 +0100

    [ST] Add more resources to log collection (#2321)
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtests/src/main/java/com/github/streamshub/systemtests/logs/TestLogCollector.java
+++ systemtests/src/main/java/com/github/streamshub/systemtests/logs/TestLogCollector.java
@@ -3,10 +3,16 @@ package com.github.streamshub.systemtests.logs;
 import com.github.streamshub.console.api.v1alpha1.Console;
 import com.github.streamshub.systemtests.Environment;
 import com.github.streamshub.systemtests.constants.Labels;
+import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.LabelSelectorBuilder;
 import io.fabric8.kubernetes.api.model.Secret;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceAccount;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.rbac.ClusterRoleBinding;
+import io.fabric8.kubernetes.api.model.rbac.RoleBinding;
+import io.fabric8.openshift.api.model.ClusterRole;
 import io.fabric8.openshift.api.model.operatorhub.v1.OperatorGroup;
 import io.fabric8.openshift.api.model.operatorhub.v1alpha1.ClusterServiceVersion;
 import io.fabric8.openshift.api.model.operatorhub.v1alpha1.InstallPlan;
@@ -55,6 +61,12 @@ public class TestLogCollector {
 
     private LogCollector defaultLogCollector() {
         List<String> resources = new ArrayList<>(List.of(
+            HasMetadata.getKind(Service.class),
+            HasMetadata.getKind(ConfigMap.class),
+            HasMetadata.getKind(ClusterRole.class),
+            HasMetadata.getKind(ClusterRoleBinding.class),
+            HasMetadata.getKind(RoleBinding.class),
+            HasMetadata.getKind(ServiceAccount.class),
             HasMetadata.getKind(Secret.class),
             HasMetadata.getKind(Deployment.class),
             HasMetadata.getKind(Console.class),
