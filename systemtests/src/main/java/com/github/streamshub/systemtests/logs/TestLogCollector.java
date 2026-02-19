027bb459c6b67408b2dfe91cec57d5e8a413ad31
jkalinic <jkalinic@redhat.com>
2026-02-19 01:34:34 +0100

    Remove serviceaccounts
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtests/src/main/java/com/github/streamshub/systemtests/logs/TestLogCollector.java
+++ systemtests/src/main/java/com/github/streamshub/systemtests/logs/TestLogCollector.java
@@ -8,7 +8,6 @@ import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.LabelSelectorBuilder;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.ServiceAccount;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.openshift.api.model.operatorhub.v1.OperatorGroup;
 import io.fabric8.openshift.api.model.operatorhub.v1alpha1.ClusterServiceVersion;
@@ -60,7 +59,6 @@ public class TestLogCollector {
         List<String> resources = new ArrayList<>(List.of(
             HasMetadata.getKind(Service.class),
             HasMetadata.getKind(ConfigMap.class),
-            HasMetadata.getKind(ServiceAccount.class),
             HasMetadata.getKind(Secret.class),
             HasMetadata.getKind(Deployment.class),
             HasMetadata.getKind(Console.class),
