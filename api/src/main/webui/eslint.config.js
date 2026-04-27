f525f9cd29d5db5236897152bc48360e459abb06
Michael Edgar <medgar@redhat.com>
2026-04-27 08:43:48 -0400

    Add eslint libraries and configuration
    Signed-off-by: Michael Edgar <medgar@redhat.com>

---
+++ api/src/main/webui/eslint.config.js
@@ -0,0 +1,8 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+);
