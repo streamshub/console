db19926c4fed3e98fd7cdfa9a16856c3333a388e
Michael Edgar <medgar@redhat.com>
2026-08-14 14:05:05 -0400

    Fix TypeScript Playwright tests for data view changes
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- ui/tests/playwright/NodesPage.test.tsx
+++ ui/tests/playwright/NodesPage.test.tsx
@@ -11,27 +11,31 @@ test("Nodes page", async ({ page, authenticatedPage }) => {
   });
   await test.step("Nodes page should display table", async () => {
     await expect(page.locator('h1').getByText('Nodes')).toBeVisible();
-    await expect(page.getByText('Node Partition Distribution')).toBeVisible();
 
     const headerRows = await page
-      .locator('table[data-ouia-component-id="nodes-listing"] thead tr')
+      .locator('table[data-ouia-component-id="nodes-table"] thead tr')
       .all();
     const headerRow = headerRows[0];
-    expect(await headerRow.locator("th").nth(1).innerText()).toBe("Node ID");
-    expect(await headerRow.locator("th").nth(2).innerText()).toBe("Roles");
-    expect(await headerRow.locator("th").nth(3).innerText()).toBe("Status");
-    expect(await headerRow.locator("th").nth(4).innerText()).toContain(
+    let col = 0;
+    expect(await headerRow.locator("th").nth(col++).innerText()).toBe("Node ID");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toBe("Roles");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toBe("Status");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toBe("Kafka version");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toContain(
       "Total Replicas ",
     );
-    expect(await headerRow.locator("th").nth(5).innerText()).toContain("Rack ");
-    expect(await headerRow.locator("th").nth(6).innerText()).toBe("Node Pool");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toContain(
+      "Leader partitions ",
+    );
+    expect(await headerRow.locator("th").nth(col++).innerText()).toContain("Rack ");
+    expect(await headerRow.locator("th").nth(col++).innerText()).toBe("Node Pool");
 
     const dataRows = await page
-      .locator('table[data-ouia-component-id="nodes-listing"] tbody tr')
+      .locator('table[data-ouia-component-id="nodes-table"] tbody tr')
       .count();
     expect(dataRows).toBeGreaterThan(0);
     const dataCells = await page
-      .locator('table[data-ouia-component-id="nodes-listing"] tbody tr td')
+      .locator('table[data-ouia-component-id="nodes-table"] tbody tr td')
       .evaluateAll((tds) => tds.map((td) => td.innerHTML?.trim() ?? ""));
 
     expect(dataCells.length).toBeGreaterThan(0);
